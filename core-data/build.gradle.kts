2b34b3e2276d55ef9b03295e463804cff8444d84
이용인 <yongin0517@gmail.com>
2024-01-30 01:43:13 +0900

    Chore: 의존성 추가

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -9,6 +9,8 @@ java {
 }
 
 dependencies {
+    implementation(project(":core-model"))
+    implementation(project(":core-repository"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 }

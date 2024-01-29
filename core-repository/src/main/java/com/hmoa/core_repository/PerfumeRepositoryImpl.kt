ae892c7e3bdbf4faf18af6c6b98c77c227ada09a
이용인 <yongin0517@gmail.com>
2024-01-30 02:49:01 +0900

    Feat: Perfume Repository 인터페이스 구현체 생성

---
+++ core-repository/src/main/java/com/hmoa/core_repository/PerfumeRepositoryImpl.kt
@@ -0,0 +1,57 @@
+package com.hmoa.core_repository
+
+import com.hmoa.core_datastore.PerfumeDataStore
+import com.hmoa.core_model.request.AgeRequestDto
+import com.hmoa.core_model.request.PerfumeGenderRequestDto
+import com.hmoa.core_model.request.PerfumeWeatherRequestDto
+import com.hmoa.core_model.response.*
+
+private class PerfumeRepositoryImpl(private val perfumeDataStore: PerfumeDataStore) : PerfumeRepository {
+    override fun getPerfumeTopDetail(perfumeId: Int): PerfumeDetailResponseDto {
+        return perfumeDataStore.getPerfumeTopDetail(perfumeId)
+    }
+
+    override fun getPerfumeBottomDetail(perfumeId: Int): PerfumeDetailSecondResponseDto {
+        return perfumeDataStore.getPerfumeBottomDetail(perfumeId)
+    }
+
+    override fun postPerfumeMainPhoto(imageUrl: String): DataResponseDto<Any> {
+        return perfumeDataStore.postPerfumeMainPhoto(imageUrl)
+    }
+
+    override fun postPerfumeAge(dto: AgeRequestDto, perfumeId: Int): PerfumeAgeResponseDto {
+        return perfumeDataStore.postPerfumeAge(dto, perfumeId)
+    }
+
+    override fun deletePerfumeAge(perfumeId: Int): PerfumeAgeResponseDto {
+        return perfumeDataStore.deletePerfumeAge(perfumeId)
+    }
+
+    override fun postPerfumeGender(dto: PerfumeGenderRequestDto, perfumeId: Int): PerfumeGenderResponseDto {
+        return perfumeDataStore.postPerfumeGender(dto, perfumeId)
+    }
+
+    override fun deletePerfumeGender(perfumeId: Int): PerfumeGenderResponseDto {
+        return perfumeDataStore.deletePerfumeGender(perfumeId)
+    }
+
+    override fun putPerfumeLike(perfumeId: Int): DataResponseDto<Any> {
+        return perfumeDataStore.putPerfumeLike(perfumeId)
+    }
+
+    override fun deletePerfumeLike(perfumeId: Int): DataResponseDto<Any> {
+        return perfumeDataStore.deletePerfumeLike(perfumeId)
+    }
+
+    override fun postPerfumeWeather(dto: PerfumeWeatherRequestDto): PerfumeWeatherResponseDto {
+        return perfumeDataStore.postPerfumeWeather(dto)
+    }
+
+    override fun deletePerfumeWeather(perfumeId: Int): PerfumeWeatherResponseDto {
+        return perfumeDataStore.deletePerfumeWeather(perfumeId)
+    }
+
+    override fun getLikePerfumeList(): DataResponseDto<Any> {
+        return perfumeDataStore.getLikePerfumeList()
+    }
+}

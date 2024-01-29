d21d7517317fbc26a995aec89aabd3834d250218
이용인 <yongin0517@gmail.com>
2024-01-29 13:45:22 +0900

    Chore: JUnit 라이브러리 추가

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -7,3 +7,8 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}

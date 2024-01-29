9170643110cf9e803a662758d43ce0c5d4e4ae6c
uselessnaming <seohojon@naver.com>
2024-01-29 16:57:35 +0900

    Chore: navigation 의존성 추가

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
 
     implementation("androidx.compose.ui:ui:1.1.0")
 
+    //bottom navigation
+    implementation("androidx.navigation:navigation-compose:2.7.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")

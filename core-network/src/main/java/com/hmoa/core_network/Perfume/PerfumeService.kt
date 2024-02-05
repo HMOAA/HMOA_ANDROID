b6989b48e6e982d61de57a8b9d60726832e99622
이용인 <yongin0517@gmail.com>
2024-02-06 00:08:41 +0900

    Fix: suspend 함수로 변경

---
+++ core-network/src/main/java/com/hmoa/core_network/Perfume/PerfumeService.kt
@@ -0,0 +1,19 @@
+package com.hmoa.core_network.Perfume
+
+import com.hmoa.core_model.request.AgeRequestDto
+import com.hmoa.core_model.request.PerfumeGenderRequestDto
+import com.hmoa.core_model.response.*
+
+interface PerfumeService {
+    suspend fun getPerfumeTopDetail(perfumeId: String): PerfumeDetailResponseDto
+    suspend fun getPerfumeBottomDetail(perfumeId: String): PerfumeDetailSecondResponseDto
+    suspend fun postPerfumeAge(dto: AgeRequestDto, perfumeId: String): PerfumeAgeResponseDto
+    suspend fun deletePerfumeAge(perfumeId: String): PerfumeAgeResponseDto
+    suspend fun postPerfumeGender(dto: PerfumeGenderRequestDto, perfumeId: String): PerfumeGenderResponseDto
+    suspend fun deletePerfumeGender(perfumeId: String): PerfumeGenderResponseDto
+    suspend fun putPerfumeLike(perfumeId: String): DataResponseDto<Any>
+    suspend fun deletePerfumeLike(perfumeId: String): DataResponseDto<Any>
+    suspend fun postPerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto
+    suspend fun deletePerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto
+    suspend fun getPerfumeLike(): DataResponseDto<Any>
+}
---
+++ core-network/src/main/java/com/hmoa/core_network/Perfume/PerfumeServiceImpl.kt
@@ -0,0 +1,68 @@
+package com.hmoa.core_network.Perfume
+
+import com.hmoa.core_model.request.AgeRequestDto
+import com.hmoa.core_model.request.PerfumeGenderRequestDto
+import com.hmoa.core_model.response.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import javax.inject.Inject
+
+class PerfumeServiceImpl @Inject constructor(
+    private val httpClient: HttpClient
+) : PerfumeService {
+
+    override suspend fun getPerfumeTopDetail(perfumeId: String): PerfumeDetailResponseDto {
+        return httpClient.get("/perfume/${perfumeId}").body()
+    }
+
+    override suspend fun getPerfumeBottomDetail(perfumeId: String): PerfumeDetailSecondResponseDto {
+        return httpClient.get("/perfume/${perfumeId}/2").body()
+    }
+
+    override suspend fun postPerfumeAge(dto: AgeRequestDto, perfumeId: String): PerfumeAgeResponseDto {
+        val response = httpClient.post("/perfume/${perfumeId}/age") {
+            url {
+                body = dto
+            }
+        }
+        return response.body()
+    }
+
+    override suspend fun deletePerfumeAge(perfumeId: String): PerfumeAgeResponseDto {
+        return httpClient.delete("/perfume/${perfumeId}/age").body()
+    }
+
+    override suspend fun postPerfumeGender(dto: PerfumeGenderRequestDto, perfumeId: String): PerfumeGenderResponseDto {
+        val response = httpClient.post("/perfume/${perfumeId}/gender") {
+            url {
+                body = dto
+            }
+        }
+        return response.body()
+    }
+
+    override suspend fun deletePerfumeGender(perfumeId: String): PerfumeGenderResponseDto {
+        return httpClient.delete("/perfume/${perfumeId}/gender").body()
+    }
+
+    override suspend fun putPerfumeLike(perfumeId: String): DataResponseDto<Any> {
+        return httpClient.put("/perfume/${perfumeId}/like").body()
+    }
+
+    override suspend fun deletePerfumeLike(perfumeId: String): DataResponseDto<Any> {
+        return httpClient.delete("/perfume/${perfumeId}/like").body()
+    }
+
+    override suspend fun postPerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto {
+        return httpClient.post("/perfume/${perfumeId}/weather").body()
+    }
+
+    override suspend fun deletePerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto {
+        return httpClient.delete("/perfume/${perfumeId}/weather").body()
+    }
+
+    override suspend fun getPerfumeLike(): DataResponseDto<Any> {
+        return httpClient.get("/perfume/like").body()
+    }
+}

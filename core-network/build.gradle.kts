af1f2891fa57542b55810acce9ed10189adeb6a2
이용인 <yongin0517@gmail.com>
2024-02-06 16:23:32 +0900

    Chore: core-network 모듈을 코틀린 라이브러리로 변경

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -1,67 +1,24 @@
 plugins {
     kotlin("kapt")
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
+    id("java-library")
 }
 
-android {
-    namespace = "com.hmoa.core_network"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-
-        buildConfigField("String", "BASE_URL", properties["BASE_URL"].toString())
-    }
-
-    buildFeatures {
-        buildConfig = true
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    kotlin {
-        sourceSets {
-            all {
-                languageSettings.optIn("io.ktor.util.InternalAPI")
-            }
-        }
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
+
 
 dependencies {
     implementation(project(":core-model"))
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-
-    implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-android-compiler:2.50")
-
-    implementation("io.ktor:ktor-client-android:2.3.7")
+    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-compiler:2.44")
     implementation("io.ktor:ktor-client-serialization:2.3.7")
     implementation("io.ktor:ktor-client-logging-jvm:2.3.7")
     implementation("io.ktor:ktor-client-core:2.3.7")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
-
+    implementation("io.ktor:ktor-client-okhttp-jvm:2.3.7")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testAnnotationProcessor("com.google.dagger:hilt-compiler:2.44")
 }

8be5c17463edc51747a0d90b32ee918696acb075
이용인 <yongin0517@gmail.com>
2024-02-08 17:59:50 +0900

    Feat: PerfumeDataStore 생성

--- core-datastore/src/main/java/com/hmoa/core_datastore/PerfumeDataStore.kt
+++ core-datastore/src/main/java/com/hmoa/core_datastore/PerfumeDataStore.kt
@@ -8,14 +8,13 @@ import com.hmoa.core_model.response.*
 interface PerfumeDataStore {
     suspend fun getPerfumeTopDetail(perfumeId: String): PerfumeDetailResponseDto
     suspend fun getPerfumeBottomDetail(perfumeId: String): PerfumeDetailSecondResponseDto
-    suspend fun postPerfumeMainPhoto(imageUrl: String): DataResponseDto<Any>
     suspend fun postPerfumeAge(dto: AgeRequestDto, perfumeId: String): PerfumeAgeResponseDto
     suspend fun deletePerfumeAge(perfumeId: String): PerfumeAgeResponseDto
     suspend fun postPerfumeGender(dto: PerfumeGenderRequestDto, perfumeId: String): PerfumeGenderResponseDto
     suspend fun deletePerfumeGender(perfumeId: String): PerfumeGenderResponseDto
     suspend fun putPerfumeLike(perfumeId: String): DataResponseDto<Any>
     suspend fun deletePerfumeLike(perfumeId: String): DataResponseDto<Any>
-    suspend fun postPerfumeWeather(dto: PerfumeWeatherRequestDto): PerfumeWeatherResponseDto
+    suspend fun postPerfumeWeather(perfumeId: String, dto: PerfumeWeatherRequestDto): PerfumeWeatherResponseDto
     suspend fun deletePerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto
-    suspend fun getLikePerfumeList(): DataResponseDto<Any>
+    suspend fun getLikePerfumes(): DataResponseDto<Any>
 }
--- core-datastore/src/main/java/com/hmoa/core_datastore/PerfumeDataStoreImpl.kt
+++ core-datastore/src/main/java/com/hmoa/core_datastore/PerfumeDataStoreImpl.kt
@@ -6,53 +6,52 @@ import com.hmoa.core_model.request.PerfumeWeatherRequestDto
 import com.hmoa.core_model.response.*
 import com.hmoa.core_network.Perfume.PerfumeService
 
-private class PerfumeDataStoreImpl constructor(private val perfumeService: PerfumeService) : PerfumeDataStore {
+internal class PerfumeDataStoreImpl constructor(private val perfumeService: PerfumeService) : PerfumeDataStore {
     override suspend fun getPerfumeTopDetail(perfumeId: String): PerfumeDetailResponseDto {
         return perfumeService.getPerfumeTopDetail(perfumeId)
     }
 
     override suspend fun getPerfumeBottomDetail(perfumeId: String): PerfumeDetailSecondResponseDto {
-        perfumeService.getPerfumeBottomDetail(perfumeId)
-    }
-
-    override suspend fun postPerfumeMainPhoto(imageUrl: String): DataResponseDto<Any> {
-        TODO("Not yet implemented")
+        return perfumeService.getPerfumeBottomDetail(perfumeId)
     }
 
     override suspend fun postPerfumeAge(dto: AgeRequestDto, perfumeId: String): PerfumeAgeResponseDto {
-        TODO("Not yet implemented")
+        return perfumeService.postPerfumeAge(dto, perfumeId)
     }
 
     override suspend fun deletePerfumeAge(perfumeId: String): PerfumeAgeResponseDto {
-        TODO("Not yet implemented")
+        return perfumeService.deletePerfumeAge(perfumeId)
     }
 
     override suspend fun postPerfumeGender(dto: PerfumeGenderRequestDto, perfumeId: String): PerfumeGenderResponseDto {
-        TODO("Not yet implemented")
+        return perfumeService.postPerfumeGender(dto, perfumeId)
     }
 
     override suspend fun deletePerfumeGender(perfumeId: String): PerfumeGenderResponseDto {
-        TODO("Not yet implemented")
+        return perfumeService.deletePerfumeGender(perfumeId)
     }
 
     override suspend fun putPerfumeLike(perfumeId: String): DataResponseDto<Any> {
-        TODO("Not yet implemented")
+        return perfumeService.putPerfumeLike(perfumeId)
     }
 
     override suspend fun deletePerfumeLike(perfumeId: String): DataResponseDto<Any> {
-        TODO("Not yet implemented")
+        return perfumeService.deletePerfumeLike(perfumeId)
     }
 
-    override suspend fun postPerfumeWeather(dto: PerfumeWeatherRequestDto): PerfumeWeatherResponseDto {
-        TODO("Not yet implemented")
+    override suspend fun postPerfumeWeather(
+        perfumeId: String,
+        dto: PerfumeWeatherRequestDto
+    ): PerfumeWeatherResponseDto {
+        return perfumeService.postPerfumeWeather(perfumeId, dto)
     }
 
     override suspend fun deletePerfumeWeather(perfumeId: String): PerfumeWeatherResponseDto {
-        TODO("Not yet implemented")
+        return perfumeService.deletePerfumeWeather(perfumeId)
     }
 
-    override suspend fun getLikePerfumeList(): DataResponseDto<Any> {
-        TODO("Not yet implemented")
+    override suspend fun getLikePerfumes(): DataResponseDto<Any> {
+        return perfumeService.getLikePerfumes()
     }
 
 }

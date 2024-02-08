935da97ccfc8f76e913d8cc939b1211e1dcf9437
이용인 <yongin0517@gmail.com>
2024-02-08 19:42:04 +0900

    Feat: CommunityCommentDataStore 생성

---
+++ core-datastore/src/main/java/com/hmoa/core_datastore/CommunityComment/CommunityCommentDataStoreImpl.kt
@@ -0,0 +1,32 @@
+package com.hmoa.core_datastore.CommunityComment
+
+import com.hmoa.core_model.request.CommunityCommentDefaultRequestDto
+import com.hmoa.core_model.response.CommunityCommentAllResponseDto
+import com.hmoa.core_model.response.CommunityCommentDefaultResponseDto
+import com.hmoa.core_model.response.DataResponseDto
+import com.hmoa.core_network.CommunityComment.CommunityCommentService
+
+internal class CommunityCommentDataStoreImpl constructor(private val communityCommentService: CommunityCommentService) :
+    CommunityCommentDataStore {
+    override suspend fun putCommunityComment(
+        commentId: Int,
+        dto: CommunityCommentDefaultRequestDto
+    ): CommunityCommentDefaultResponseDto {
+        return communityCommentService.putCommunityComment(commentId, dto)
+    }
+
+    override suspend fun deleteCommunityComment(commentId: Int): DataResponseDto<Any> {
+        return communityCommentService.deleteCommunityComment(commentId)
+    }
+
+    override suspend fun postCommunityComment(commentId: Int, page: String): CommunityCommentAllResponseDto {
+        return communityCommentService.postCommunityComment(commentId, page)
+    }
+
+    override suspend fun postCommunityComment(
+        commentId: Int,
+        dto: CommunityCommentDefaultRequestDto
+    ): CommunityCommentDefaultResponseDto {
+        return communityCommentService.postCommunityComment(commentId, dto)
+    }
+}

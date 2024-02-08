eca63bf79251832b74a4949c939e12fbc2138b2c
이용인 <yongin0517@gmail.com>
2024-02-08 19:39:10 +0900

    Feat: CommunityDataStore 생성

---
+++ core-datastore/src/main/java/com/hmoa/core_datastore/Community/CommunityDataStoreImpl.kt
@@ -0,0 +1,46 @@
+package com.hmoa.core_datastore.Community
+
+import com.hmoa.core_model.Category
+import com.hmoa.core_model.response.CommunityByCategoryResponseDto
+import com.hmoa.core_model.response.CommunityDefaultResponseDto
+import com.hmoa.core_model.response.DataResponseDto
+import com.hmoa.core_network.Community.CommunityService
+import java.io.File
+
+internal class CommunityDataStoreImpl constructor(private val communityService: CommunityService) : CommunityDataStore {
+    override suspend fun getCommunity(communityId: Int): CommunityDefaultResponseDto {
+        return communityService.getCommunity(communityId)
+    }
+
+    override suspend fun postCommunityUpdate(
+        images: Array<File>,
+        deleteCommunityPhotoIds: Array<Int>,
+        title: String,
+        content: String,
+        communityId: Int
+    ): CommunityDefaultResponseDto {
+        return communityService.postCommunityUpdate(images, deleteCommunityPhotoIds, title, content, communityId)
+    }
+
+    override suspend fun deleteCommunity(communityId: Int): DataResponseDto<Any> {
+        return communityService.deleteCommunity(communityId)
+    }
+
+    override suspend fun getCommunityByCategory(category: Category, page: String): CommunityByCategoryResponseDto {
+        return communityService.getCommunityByCategory(category, page)
+    }
+
+    override suspend fun getCommunitiesHome(): List<CommunityByCategoryResponseDto> {
+        return communityService.getCommunitiesHome()
+    }
+
+    override suspend fun postCommunitySave(
+        images: Array<File>,
+        category: Category,
+        title: String,
+        content: String
+    ): CommunityDefaultResponseDto {
+        return communityService.postCommunitySave(images, category, title, content)
+    }
+
+}

4d47d0ed51fb15824d041057f155762bdd4cc5c7
uselessnaming <seohojon@naver.com>
2024-02-19 19:36:43 +0900

    Chore: compose 라이브러리 설정

--- userInfo/build.gradle.kts
+++ userInfo/build.gradle.kts
@@ -32,17 +32,19 @@ android {
     buildFeatures{
         compose = true
     }
-    packaging{
-        resources{
-            excludes += "META-INF/gradle/incremental.annotation.processors"
-        }
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

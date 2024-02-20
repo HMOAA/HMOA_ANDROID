965709102c80064ea7218448eea0cfa753066151
uselessnaming <seohojon@naver.com>
2024-02-20 14:39:53 +0900

    Feat: No Authentication My Page UI 추가

--- userInfo/src/main/java/com/example/userinfo/MyPage.kt
+++ userInfo/src/main/java/com/example/userinfo/MyPage.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.hmoa.userinfo.R
 
 @Composable
 fun MyPage(){
---
+++ userInfo/src/main/java/com/example/userinfo/NoAuthMyPage.kt
@@ -0,0 +1,31 @@
+package com.example.userinfo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoAuthMyPage(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color.White)
+            .padding(horizontal = 16.dp)
+    ){
+        Spacer(Modifier.height(126.dp))
+
+    }
+}
+
+@Preview
+@Composable
+fun TestNoAuthMyPage(){
+    NoAuthMyPage()
+}

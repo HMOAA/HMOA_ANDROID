df6ed1c05223e57eb6664205f9bde5cb2748c6b6
이용인 <yongin0517@gmail.com>
2024-02-21 00:09:57 +0900

    Chore: 컴파일 버전 33 -> 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hmoa.app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hmoa.app"
--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hmoa.sample_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hmoa.sample_app"

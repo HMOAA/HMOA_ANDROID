1df1e2feb98d631997cbf9dc3dcb1957d47df4cc
이용인 <yongin0517@gmail.com>
2024-02-24 04:40:25 +0900

    Chore: datastore 라이브러리 추가

--- core-database/build.gradle.kts
+++ core-database/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")

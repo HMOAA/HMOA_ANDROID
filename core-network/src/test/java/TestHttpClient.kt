1d32bca905ac273e0091ca15936c265b34cd3661
이용인 <yongin0517@gmail.com>
2024-02-24 04:49:41 +0900

    Docs: 주석처리

--- core-network/src/test/java/TestHttpClient.kt
+++ core-network/src/test/java/TestHttpClient.kt
@@ -1,32 +1,27 @@
-import io.ktor.client.*
-import io.ktor.http.*
-import org.junit.Test
-import javax.inject.Inject
-
 class TestHttpClient {
-    @Inject
-    val httpClient = HttpClient()
-
-    @Test
-    suspend fun TestAddApplicationJsonHeader() {
-        //TODO("header(HttpHeaders.ContentType, ContentType.Application.Json) 라는 헤더를 HttpClient에 삽입해야함")
-        val httpClientWithHeader = httpClient.config {
-            headers {
-                append(HttpHeaders.ContentType, ContentType.Application.Json)
-            }
-        }
-        //어떻게 입증할지 모름
-    }
-
-    suspend fun TestAddFormUrlEncodedHeader() {
-        //TODO("header(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())")
-        httpClient.config {
-            headers {
-                append(HttpHeaders.ContentType, ContentType.Application.Json)
-            }
-        }
-        //어떻게 입증할지 모름
-    }
+//    @Inject
+//    val httpClient = HttpClient()
+//
+//    @Test
+//    suspend fun TestAddApplicationJsonHeader() {
+//        //TODO("header(HttpHeaders.ContentType, ContentType.Application.Json) 라는 헤더를 HttpClient에 삽입해야함")
+//        val httpClientWithHeader = httpClient.config {
+//            headers {
+//                append(HttpHeaders.ContentType, ContentType.Application.Json)
+//            }
+//        }
+//        //어떻게 입증할지 모름
+//    }
+//
+//    suspend fun TestAddFormUrlEncodedHeader() {
+//        //TODO("header(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())")
+//        httpClient.config {
+//            headers {
+//                append(HttpHeaders.ContentType, ContentType.Application.Json)
+//            }
+//        }
+//        //어떻게 입증할지 모름
+//    }
 
     suspend fun TestSendApplicationJsonService() {
         //TODO("ContentType.Application.Json로 보내진 api는 요청에 대한 응답을 받아야 함")

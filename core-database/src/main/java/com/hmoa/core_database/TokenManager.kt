8716e17360e67adcf9544aefb7e2f748add88611
이용인 <yongin0517@gmail.com>
2024-02-25 08:52:44 +0900

    Feat: DataStore 및 TokenManager 생성

---
+++ core-database/src/main/java/com/hmoa/core_database/TokenManager.kt
@@ -0,0 +1,33 @@
+package com.hmoa.core_database
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class TokenManager @Inject constructor(private val dataStore: DataStore<Preferences>) {
+    companion object {
+        private val AUTH_TOKEN_KEY = stringPreferencesKey("AUTH_TOKEN")
+    }
+
+    fun getAuthToken(): Flow<String?> {
+        return dataStore.data.map { preferences ->
+            preferences[AUTH_TOKEN_KEY]
+        }
+    }
+
+    suspend fun saveAccessToken(token: String) {
+        dataStore.edit { preferences ->
+            preferences[AUTH_TOKEN_KEY] = token
+        }
+    }
+
+    suspend fun deleteAccessToken() {
+        dataStore.edit { preferences ->
+            preferences.remove(AUTH_TOKEN_KEY)
+        }
+    }
+}

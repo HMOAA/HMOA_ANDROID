62dd2afccea7eba43e1b4c297f2b234d9e692f58
이용인 <yongin0517@gmail.com>
2024-02-26 21:59:18 +0900

    Feat: Application 설정
    - 앱 다크모드 취소
    - kakaoSdk 초기화

--- app/src/main/java/com/hmoa/app/App.kt
+++ app/src/main/java/com/hmoa/app/App.kt
@@ -1,9 +1,13 @@
 package com.hmoa.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
 
 class App : Application() {
     override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate()
+        KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
     }
 }

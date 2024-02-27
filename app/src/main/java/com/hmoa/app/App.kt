d0293a541a9b763a0606fde29f7cc360a81568f5
이용인 <yongin0517@gmail.com>
2024-02-27 13:33:12 +0900

    Docs: hilt 어노테이션 추가

--- app/src/main/java/com/hmoa/app/App.kt
+++ app/src/main/java/com/hmoa/app/App.kt
@@ -3,7 +3,9 @@ package com.hmoa.app
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)

8a0caea50e0914106b3dc70e602cb3c7b4df9798
이용인 <yongin0517@gmail.com>
2024-02-27 23:45:07 +0900

    Remove

--- feature-authentication/src/main/java/com/hmoa/feature_authentication/navigation/AuthenticationNavigation.kt
+++ feature-authentication/src/main/java/com/hmoa/feature_authentication/navigation/AuthenticationNavigation.kt
@@ -2,9 +2,7 @@ package com.hmoa.feature_authentication.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.hmoa.feature_authentication.LoginRoute
 import com.hmoa.feature_authentication.PickNicknameRoute
 import com.hmoa.feature_authentication.SignupRoute
@@ -14,8 +12,6 @@ const val LOGIN_ROUTE = "login_route"
 const val SIGNUP_ROUTE = "signup_route"
 const val PICKNICKNAME_ROUTE = "pickNickname_route"
 const val PICKPERSONALINFO_ROUTE = "pickPersonalInto_route"
-
-fun NavController.navigateToAuthenticationGraph(navOptions: NavOptions) = navigate(AUTHENTICATION_GRAPH, navOptions)
 fun NavController.navigateToLogin() = navigate(LOGIN_ROUTE)
 fun NavController.navigateToSignup() = navigate(SIGNUP_ROUTE)
 fun NavController.navigateToPickNickname() = navigate(PICKNICKNAME_ROUTE)

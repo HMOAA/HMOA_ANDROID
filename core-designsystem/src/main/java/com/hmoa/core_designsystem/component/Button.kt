b09d6492052d4e86905a5ccaaee1152d6058b4f9
이용인 <yongin0517@gmail.com>
2024-02-27 23:35:27 +0900

    Fix: 이름 변경 및 프로퍼티 추가

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/Button.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/Button.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,27 +19,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun BottomButton(
+fun Button(
     isEnabled : Boolean, //버튼 활성화 여부
     btnText : String, //버튼 메세지
     onClick : () -> Unit, //버튼 이벤트
+    buttonModifier: Modifier? = null,
+    textColor: Color = Color.White,
+    textSize:Int = 20,
+    radious:Int = 0
 ){
     Row(
-        modifier = Modifier.fillMaxWidth()
-            .height(80.dp)
-            .background(color = if(isEnabled) Color.Black else Color(0xFFBBBBBB))
+        modifier = Modifier
+            .background(color = if(isEnabled) Color.Black else Color(0xFFBBBBBB), shape = RoundedCornerShape(radious))
             .clickable{
                 if (isEnabled){
                     onClick()
                 }
-            },
+            }.addModifier(buttonModifier),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ){
         Text(
             text = btnText,
-            fontSize = 20.sp,
-            color = Color.White
+            fontSize = textSize.sp,
+            color = textColor
         )
     }
 }
@@ -54,7 +57,7 @@ fun TestBottomButton(){
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ){
-        BottomButton(
+        Button(
             isEnabled = false,
             btnText = "다음",
             onClick = {

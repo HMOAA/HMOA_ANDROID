f651cab76513dc46a7e7e327bb39d59fd4431d1f
이용인 <yongin0517@gmail.com>
2024-03-04 03:52:47 +0900

    Chore: hilt설정 추가

--- core-database/build.gradle.kts
+++ core-database/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {

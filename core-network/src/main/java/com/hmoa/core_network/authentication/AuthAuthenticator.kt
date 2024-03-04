d5eb19900d6c826b4007d361971f94d668617daf
이용인 <yongin0517@gmail.com>
2024-03-05 01:10:29 +0900

    Feat: AuthAuthenticator 토큰 리프레싱 로직 추가

--- core-network/src/main/java/com/hmoa/core_network/authentication/AuthAuthenticator.kt
+++ core-network/src/main/java/com/hmoa/core_network/authentication/AuthAuthenticator.kt
@@ -1,7 +1,14 @@
 package com.hmoa.core_network.authentication
 
 import com.hmoa.core_database.TokenManager
+import com.hmoa.core_model.request.RememberedLoginRequestDto
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import okhttp3.Authenticator
 import okhttp3.Request
@@ -11,7 +18,10 @@ import javax.inject.Inject
 
 class AuthAuthenticator @Inject constructor(
     private val tokenManager: TokenManager,
+    private val refreshTokenManager: RefreshTokenManager
 ) : Authenticator {
+    private lateinit var refreshAuthTokenResponse: HttpResponse
+    private lateinit var newRequest: Request
     override fun authenticate(route: Route?, response: Response): Request? {
         val rememberedToken = runBlocking {
             tokenManager.getRememberedToken().first()
@@ -22,12 +32,27 @@ class AuthAuthenticator @Inject constructor(
             return null
         }
 
-        val refreshedAuthToken = suspend { //TODO("토큰리프레시 재요청 필요 RefreshTokenService에서 할 예정")}
-            //val refreshedRequest = response.request.addRefreshAuthToken(refreshedAuthToken.toString())
-            //return refreshedRequest
+        //함수화 해야함. 토큰 리프레싱 요청한 뒤 갱신된 토큰으로 재요청 보내는 것.
+        runBlocking {
+            refreshAuthTokenResponse = flow {
+                emit(refreshTokenManager.refreshAuthToken(RememberedLoginRequestDto(rememberedToken)))
+            }.first()
+
         }
-        return null
+
+        if (refreshAuthTokenResponse.status.value == HttpStatusCode.Unauthorized.value) {
+            //TODO("로그인 화면으로 이동")
+            return null
+        }
+
+        CoroutineScope(Dispatchers.IO).async {
+            val refreshedAuthToken = tokenManager.getAuthToken().first() ?: return@async null
+            newRequest = response.request.addRefreshAuthToken(refreshedAuthToken)
+        }
+
+        return newRequest
     }
+
 
     fun Request.addRefreshAuthToken(token: String): Request {
         return this.newBuilder().header("X-AUTH-TOKEN", "${token}").build()

f7b80318ac3aa88ece5ea521aba5cd433008a088
이용인 <yongin0517@gmail.com>
2024-03-08 01:29:18 +0900

    Rename: datastore preference 객체 주입 모듈 이름 변경

--- core-datastore/src/main/java/com/hmoa/core_datastore/LocalDataStorePreferenceManagerModule.kt
+++ core-datastore/src/main/java/com/hmoa/core_datastore/LocalDataStorePreferenceManagerModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface TokenManagerModule {
+interface LocalDataStorePreferenceManagerModule {
 
     @Binds
     @Singleton

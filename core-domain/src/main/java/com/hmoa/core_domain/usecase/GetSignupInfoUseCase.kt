905e90b7365b881a835025315c141865cd7f1996
이용인 <yongin0517@gmail.com>
2024-03-08 02:04:15 +0900

    Feat: GetSignupInfoUseCase 생성

---
+++ core-domain/src/main/java/com/hmoa/core_domain/usecase/GetSignupInfoUseCase.kt
@@ -0,0 +1,20 @@
+package com.hmoa.core_domain.usecase
+
+import com.hmoa.core_domain.repository.SignupRepository
+import javax.inject.Inject
+
+class GetSignupInfoUseCase @Inject constructor(private val signupRepository: SignupRepository) {
+
+    suspend fun getNickName(): String? {
+        return signupRepository.getAge()
+    }
+
+    suspend fun getSex(): String? {
+        return signupRepository.getSex()
+    }
+
+    suspend fun getAge(): String? {
+        return signupRepository.getAge()
+    }
+
+}

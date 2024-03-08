41cc03fed65869da7b90ac84652f12180a971533
이용인 <yongin0517@gmail.com>
2024-03-09 06:32:23 +0900

    Rename: ExceptionResponseDto -> ErrorResponseDto

--- core-model/src/main/java/com/hmoa/core_model/response/ErrorResponseDto.kt
+++ core-model/src/main/java/com/hmoa/core_model/response/ErrorResponseDto.kt
@@ -3,7 +3,7 @@ package com.hmoa.core_model.response
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ExceptionResponseDto(
+data class ErrorResponseDto(
     val code: String,
     val message: String
 )

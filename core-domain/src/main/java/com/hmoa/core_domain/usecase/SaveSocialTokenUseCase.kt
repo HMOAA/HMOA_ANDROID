85f8b763f609d8832ad4b3cac2a09ed6b73ab95b
이용인 <yongin0517@gmail.com>
2024-03-09 06:34:36 +0900

    Rename: SaveSocialToken -> SaveSocialTokenUseCase

--- core-domain/src/main/java/com/hmoa/core_domain/usecase/SaveSocialTokenUseCase.kt
+++ core-domain/src/main/java/com/hmoa/core_domain/usecase/SaveSocialTokenUseCase.kt
@@ -1,4 +1,12 @@
 package com.hmoa.core_domain.usecase
 
-class SaveSocialToken {
+import com.hmoa.core_domain.repository.LoginRepository
+import javax.inject.Inject
+
+class SaveSocialTokenUseCase @Inject constructor(private val loginRepository: LoginRepository) {
+    suspend fun saveKakaoAccessToken(token: String) {
+        loginRepository.saveKakaoAccessToken(token)
+    }
+
+    suspend fun saveGoogleAccessToken(token: String) {}
 }

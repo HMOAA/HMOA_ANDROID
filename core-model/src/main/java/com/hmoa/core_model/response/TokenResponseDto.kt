778d240c0dc6fd5449be38d98f7de8235f5e9ea9
이용인 <yongin0517@gmail.com>
2024-03-09 17:39:24 +0900

    Fix: val -> var 변경

--- core-model/src/main/java/com/hmoa/core_model/response/TokenResponseDto.kt
+++ core-model/src/main/java/com/hmoa/core_model/response/TokenResponseDto.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TokenResponseDto(
-    val authToken: String,
-    val rememberedToken: String
+    var authToken: String,
+    var rememberedToken: String
 )

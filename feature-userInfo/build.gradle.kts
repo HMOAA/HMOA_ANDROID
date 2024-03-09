56e9dcdc336dddd83008e0e1cffbc3289058424e
이용인 <yongin0517@gmail.com>
2024-03-09 19:58:01 +0900

    Chore: 패키지 아이디 수정

--- feature-userInfo/build.gradle.kts
+++ feature-userInfo/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("dagger.hilt.android.plugin")
     id("com.google.dagger.hilt.android")

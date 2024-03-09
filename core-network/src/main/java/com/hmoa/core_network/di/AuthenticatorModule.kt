f236f3a0ff373b39a72376fca642fcd52ae7eac8
이용인 <yongin0517@gmail.com>
2024-03-09 17:46:09 +0900

    Remove: Interceptor hilt 주입객체 삭제

--- core-network/src/main/java/com/hmoa/core_network/di/AuthenticatorModule.kt
+++ core-network/src/main/java/com/hmoa/core_network/di/AuthenticatorModule.kt
@@ -1,7 +1,6 @@
 package com.hmoa.core_network.di
 
 import com.hmoa.core_network.authentication.AuthAuthenticator
-import com.hmoa.core_network.authentication.AuthInterceptor
 import com.hmoa.core_network.authentication.RefreshTokenManager
 import com.hmoa.core_network.authentication.RefreshTokenManagerImpl
 import dagger.Binds
@@ -9,7 +8,6 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Authenticator
-import okhttp3.Interceptor
 import javax.inject.Singleton
 
 @Module
@@ -19,12 +17,6 @@ interface AuthenticatorModule {
     @Binds
     @Singleton
     fun bindAuthenticatorImpl(authAuthenticator: AuthAuthenticator): Authenticator
-
-
-    @Binds
-    @Singleton
-    fun bindAuthInterceptor(authInterceptor: AuthInterceptor): Interceptor
-
 
     @Binds
     @Singleton

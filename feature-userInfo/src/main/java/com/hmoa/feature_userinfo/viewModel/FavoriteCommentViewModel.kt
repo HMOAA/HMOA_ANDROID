562fe8bc18b971e6ce2875d0e58346e91d1d20c7
이용인 <yongin0517@gmail.com>
2024-03-09 19:58:33 +0900

    Fix: ViewModel 상속 누락 추가

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/FavoriteCommentViewModel.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/FavoriteCommentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.feature_userinfo.viewModel
 
+import androidx.lifecycle.ViewModel
 import com.hmoa.core_domain.repository.MemberRepository
 import com.hmoa.core_model.response.CommunityCommentDefaultResponseDto
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,7 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class FavoriteCommentViewModel @Inject constructor(
     private val repository: MemberRepository
-) {
+) : ViewModel() {
 
     //좋아요 누른 댓글 목록
     private var _favoriteCommentList: Flow<List<CommunityCommentDefaultResponseDto>> = flowOf(emptyList())

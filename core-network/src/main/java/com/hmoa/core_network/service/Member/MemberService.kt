89e81a1842d547d17687b6e4815287db10686eb7
uselessNaming <seohojon@naver.com>
2024-03-10 23:30:46 +0900

    Fix: Dto 수정

--- core-network/src/main/java/com/hmoa/core_network/service/Member/MemberService.kt
+++ core-network/src/main/java/com/hmoa/core_network/service/Member/MemberService.kt
@@ -11,12 +11,13 @@ interface MemberService {
     suspend fun updateAge(request: AgeRequestDto): DataResponseDto<Any>
     suspend fun getCommunities(page: Int): List<CommunityByCategoryResponseDto>
     suspend fun getCommunityComments(page: Int): List<CommunityCommentDefaultResponseDto>
+    suspend fun getCommunityFavoriteComments(page : Int) : List<CommunityCommentDefaultResponseDto>
     suspend fun deleteMember(): DataResponseDto<Any>
     suspend fun postExistsNickname(request: NickNameRequestDto): DataResponseDto<Any>
-    suspend fun getHearts(page: Int): List<CommunityCommentDefaultResponseDto>
     suspend fun updateJoin(request: JoinUpdateRequestDto): MemberResponseDto
     suspend fun updateNickname(request: NickNameRequestDto): DataResponseDto<Any>
     suspend fun getPerfumeComments(page: Int): List<CommunityCommentDefaultResponseDto>
+    suspend fun getPerfumeFavoriteComments(page : Int) : List<CommunityCommentDefaultResponseDto>
     suspend fun postProfilePhoto(image: String): DataResponseDto<Any>
     suspend fun deleteProfilePhoto(): DataResponseDto<Any>
     suspend fun updateSex(request: SexRequestDto): DataResponseDto<Any>

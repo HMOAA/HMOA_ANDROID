40b86f4ef6aab48f46c00775c569e6e855842f17
이용인 <yongin0517@gmail.com>
2024-03-10 17:47:56 +0900

    Fix: PickPersonalInfoScreen 기능 생성

--- feature-authentication/src/main/java/com/hmoa/feature_authentication/viewmodel/PickPersonalInfoViewmodel.kt
+++ feature-authentication/src/main/java/com/hmoa/feature_authentication/viewmodel/PickPersonalInfoViewmodel.kt
@@ -1,12 +1,17 @@
 package com.hmoa.feature_authentication.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.hmoa.core_common.Result
+import com.hmoa.core_common.asResult
 import com.hmoa.core_domain.usecase.GetSignupInfoUseCase
 import com.hmoa.core_domain.usecase.PostSignupInfoUseCase
-import com.hmoa.core_domain.usecase.SaveSignupInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -14,59 +19,65 @@ import javax.inject.Inject
 @HiltViewModel
 class PickPersonalInfoViewmodel @Inject constructor(
     private val getSignupInfoUseCase: GetSignupInfoUseCase,
-    private val saveSignupInfoUseCase: SaveSignupInfoUseCase,
-    private val postSignupInfoUseCase: PostSignupInfoUseCase
+    private val postSignupInfoUseCase: PostSignupInfoUseCase,
 ) : ViewModel() {
     private val scope = CoroutineScope(Dispatchers.IO)
+    private val _birthYearState = MutableStateFlow<Int?>(null)
+    var birthYearState = _birthYearState.asStateFlow()
+    private val _sexState = MutableStateFlow("여성")
+    var sexState = _sexState.asStateFlow()
+    private val _isPostComplete = MutableStateFlow(false)
+    var isPostComplete = _isPostComplete.asStateFlow()
 
-
-    suspend fun getSavedNickname(): String? {
+    private suspend fun getSavedNickname(): String? {
         return getSignupInfoUseCase.getNickName()
     }
 
-    suspend fun getSavedAge(): String? {
-        return getSignupInfoUseCase.getAge()
-    }
-
-    suspend fun getSavedSex(): String? {
-        return getSignupInfoUseCase.getSex()
-    }
-
-    fun saveAge(value: String) {
-        scope.launch { saveSignupInfoUseCase.saveAge(value) }
+    fun saveBirthYear(value: Int) {
+        _birthYearState.update { value }
     }
 
     fun saveSex(value: String) {
-        scope.launch { saveSignupInfoUseCase.saveSex(value) }
+        _sexState.update { value }
     }
 
-    fun postSignup(birthYear: String, sex: String) {
+    fun postSignup(birthYear: Int?, sex: String) {
+        if (birthYear == null) return
         scope.launch {
             val age = mapBirthYearToAge(birthYear)
             val sex = mapSexToBoolean(sex)
             val nickname = getSavedNickname()
             if (isNicknameNotNull(nickname) && isAvailableAge(age)) {
-                postSignupInfoUseCase(age, sex, nickname = nickname!!)
+                postSignupInfoUseCase(age, sex, nickname = nickname!!).asResult().map { result ->
+                    when (result) {
+                        is Result.Success -> {
+                            _isPostComplete.update { true }
+                        }
+
+                        is Result.Loading -> TODO("로딩화면")
+                        is Result.Error -> TODO()
+                    }
+                }
             }
         }
     }
 
-    fun mapBirthYearToAge(birthYear: String): Int {
-        val age = Calendar.YEAR - birthYear.toInt()
+    private fun mapBirthYearToAge(birthYear: Int): Int {
+        val age = Calendar.YEAR - birthYear
         return age
     }
 
-    fun isNicknameNotNull(nickname: String?): Boolean {
+    private fun isNicknameNotNull(nickname: String?): Boolean {
         if (nickname == null) return false
         return true
     }
 
-    fun isAvailableAge(age: Int): Boolean {
+    private fun isAvailableAge(age: Int): Boolean {
         if (age > 0) return true
         return false
     }
 
-    fun mapSexToBoolean(sex: String): Boolean {
+    private fun mapSexToBoolean(sex: String): Boolean {
         if (sex == "여성") return false
         return true
     }

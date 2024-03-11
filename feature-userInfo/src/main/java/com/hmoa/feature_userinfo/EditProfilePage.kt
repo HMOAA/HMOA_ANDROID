5952f26b944e4d7d429fd670773e99f45b1b7262
uselessNaming <seohojon@naver.com>
2024-03-11 23:52:33 +0900

    Comment: 주석 수정

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/EditProfilePage.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/EditProfilePage.kt
@@ -1,5 +1,6 @@
 package com.example.feature_userinfo
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -26,6 +27,9 @@ fun EditProfileRoute(
     onNavBack: () -> Unit,
     viewModel: UserViewModel = hiltViewModel()
 ) {
+
+
+
     EditProfilePage(
         onNavBack = onNavBack
     )
@@ -70,7 +74,7 @@ fun EditProfilePage(
                 contentAlignment = Alignment.BottomEnd
             ) {
                 /** profile 이미지 정보 match */
-                Icon(
+                Image(
                     modifier = Modifier
                         .fillMaxSize()
                         .background(color = Color(0xFFBBBBBB), shape = CircleShape),

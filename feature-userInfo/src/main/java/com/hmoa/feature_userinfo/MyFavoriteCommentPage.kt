9b3643a316e7c3efb9126a92524519da627a3cbe
uselessNaming <seohojon@naver.com>
2024-03-11 11:49:16 +0900

    Feat: Favorite Comment View Model, UI 연결

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/MyFavoriteCommentPage.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/MyFavoriteCommentPage.kt
@@ -3,39 +3,65 @@ package com.hmoa.feature_userinfo
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.feature_userinfo.viewModel.FavoriteCommentUiState
+import com.example.feature_userinfo.viewModel.FavoriteCommentViewModel
 import com.hmoa.component.TopBar
+import com.hmoa.core_designsystem.component.Comment
+import com.hmoa.feature_userinfo.viewModel.CommentUiState
 
 @Composable
 fun MyFavoriteCommentRoute(
     onNavBack: () -> Unit,
-    onNavCommunity: () -> Unit
+    onNavCommunity: () -> Unit,
+    viewModel : FavoriteCommentViewModel = hiltViewModel()
 ) {
+
+    //comment list
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    //댓글 타입
+    val commentType by viewModel.type.collectAsStateWithLifecycle()
+
+
     MyFavoriteCommentPage(
         onNavBack = onNavBack,
-        onNavCommunity = onNavCommunity
+        onNavCommunity = onNavCommunity,
+        uiState = uiState,
+        commentType = commentType,
+        onAddPage = {
+            viewModel.addPage()
+        },
+        onTypeChanged = {
+            viewModel.changeType(it)
+        }
     )
 }
 
 @Composable
 fun MyFavoriteCommentPage(
     onNavBack: () -> Unit,
-    onNavCommunity: () -> Unit //Community로 이동?
+    onNavCommunity: () -> Unit, //Community로 이동?
+    uiState : FavoriteCommentUiState,
+    commentType : String,
+    onAddPage: () -> Unit,
+    onTypeChanged: (newType: String) -> Unit,
 ) {
-
-    //좋아요를 누른 comment 리스트
-    val commentList = listOf<Any>()
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -47,29 +73,47 @@ fun MyFavoriteCommentPage(
             onNavClick = onNavBack //뒤로 가기
         )
 
-        Spacer(Modifier.height(16.dp))
+        when(uiState) {
+            FavoriteCommentUiState.Loading -> {
+                /** Loading 화면 띄우기 */
+            }
+            else -> {
+                Spacer(Modifier.height(16.dp))
 
-        //좋아요를 누른 댓글이 있으면 LazyColumn, 없으면 No Data Page
-        if (commentList.isNotEmpty()) {
-            LazyColumn {
-                items(commentList) { comment ->
-                    /** Comment에 데이터 매칭 */
-                    /** Comment Click >> Community로 이동 */
-//                Comment(
-//                    profile = ,
-//                    nickname = ,
-//                    dateDiff = ,
-//                    comment = ,
-//                    isFirst = ,
-//                    viewNumber =
-//                )
+                /** PL되면 TypeButton 2개 추가 */
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    /** PL되면 TypeButton 2개 추가 */
+                }
+
+                when (uiState) {
+                    is FavoriteCommentUiState.Comments -> {
+                        LazyColumn {
+                            items(uiState.comments){ comment ->
+                                /** Comment 클릭 시 해당 댓글이 있는 Community로 이동 */
+                                Comment(
+                                    profile = comment.profileImg,
+                                    nickname = comment.nickname,
+                                    dateDiff = comment.createAt,
+                                    comment = comment.content,
+                                    isFirst = false,
+                                    viewNumber = if (comment.heartCount > 999) "999+" else comment.heartCount.toString(),
+                                )
+                            }
+                        }
+                    }
+                    FavoriteCommentUiState.Empty -> {
+                        NoDataPage(
+                            mainMsg = "작성한 댓글이\n없습니다",
+                            subMsg = "좋아하는 함수에 댓글을 작성해주세요"
+                        )
+                    }
+                    else -> {
+
+                    }
                 }
             }
-        } else {
-            NoDataPage(
-                mainMsg = "좋아요를 누른 댓글이\n없습니다.",
-                subMsg = "좋아하는 함수에 좋아요를 눌러주세요"
-            )
         }
     }
 }
@@ -80,5 +124,13 @@ fun TestMyFavoriteCommentPage() {
     MyFavoriteCommentPage(
         onNavBack = {},
         onNavCommunity = {},
+        uiState = FavoriteCommentUiState.Loading,
+        commentType = "Perfume",
+        onAddPage = {
+
+        },
+        onTypeChanged = {
+
+        }
     )
 }
--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/FavoriteCommentViewModel.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/FavoriteCommentViewModel.kt
@@ -1,21 +1,92 @@
 package com.example.feature_userinfo.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hmoa.core_domain.repository.MemberRepository
+import com.hmoa.core_domain.usecase.GetMyFavoriteCommentByPerfumeUseCase
+import com.hmoa.core_domain.usecase.GetMyFavoriteCommentByPostUseCase
 import com.hmoa.core_model.response.CommunityCommentDefaultResponseDto
+import com.hmoa.feature_userinfo.viewModel.CommentUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteCommentViewModel @Inject constructor(
-    private val repository: MemberRepository
+    private val repository: MemberRepository,
+    private val favoriteCommentsByCommunity : GetMyFavoriteCommentByPostUseCase,
+    private val favoriteCommentsByPerfume : GetMyFavoriteCommentByPerfumeUseCase
 ) : ViewModel() {
 
-    //좋아요 누른 댓글 목록
-    private var _favoriteCommentList: Flow<List<CommunityCommentDefaultResponseDto>> = flowOf(emptyList())
-    val favoriteCommentList: Flow<List<CommunityCommentDefaultResponseDto>>
-        get() = _favoriteCommentList
+    //선택된 type
+    private val _type = MutableStateFlow("Perfume")
+    val type get() = _type.asStateFlow()
 
+    //현재 page
+    private val _page = MutableStateFlow(0)
+    val page get() = _page.asStateFlow()
+
+    //comment 리스트
+    private val _comments = MutableStateFlow(emptyList<CommunityCommentDefaultResponseDto>())
+    val comments get() = _comments.asStateFlow()
+
+    val uiState: StateFlow<FavoriteCommentUiState> = combine(
+        type,
+        comments,
+        FavoriteCommentUiState::Comments
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(3_000),
+        initialValue = FavoriteCommentUiState.Loading
+    )
+
+    //page 증가
+    fun addPage(){
+        _page.update{_page.value + 1}
+        updateComments()
+    }
+
+    //type 변환
+    fun changeType(newType : String){
+        if (_type.value != newType) {
+            _type.update{newType}
+        }
+        updateComments()
+    }
+
+    //comment list 업데이트
+    private fun updateComments(){
+        viewModelScope.launch(Dispatchers.IO){
+            if (type.value == "Perfume") {
+                favoriteCommentsByPerfume(page.value)
+            } else {
+                favoriteCommentsByCommunity(page.value)
+            }.map{
+                _comments.update{it}
+            }
+        }
+    }
+
+}
+
+sealed interface FavoriteCommentUiState{
+    data object Loading : FavoriteCommentUiState
+
+    data class Comments(
+        val type : String,
+        val comments : List<CommunityCommentDefaultResponseDto>
+    ) : FavoriteCommentUiState
+
+    data object Empty : FavoriteCommentUiState
 }

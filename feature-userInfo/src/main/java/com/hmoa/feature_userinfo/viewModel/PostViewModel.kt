8a2796fab4f28b0c7d37ae8aff820b50c86d9306
uselessNaming <seohojon@naver.com>
2024-03-11 12:32:13 +0900

    Feat: 게시글 화면, View Model 연결

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/PostViewModel.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/PostViewModel.kt
@@ -1,21 +1,86 @@
 package com.example.feature_userinfo.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hmoa.core_domain.repository.MemberRepository
+import com.hmoa.core_domain.usecase.GetMyPostUseCase
 import com.hmoa.core_model.response.CommunityByCategoryResponseDto
+import com.hmoa.core_model.response.CommunityCommentDefaultResponseDto
+import com.hmoa.feature_userinfo.viewModel.CommentUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PostViewModel @Inject constructor(
-    private val repository: MemberRepository
+    private val postUseCase: GetMyPostUseCase
 ) : ViewModel() {
 
-    //작성한 게시글
-    private var _postList: Flow<List<CommunityByCategoryResponseDto>> = flowOf(emptyList())
-    val postList: Flow<List<CommunityByCategoryResponseDto>>
-        get() = _postList
+    //선택된 type
+    private val _type = MutableStateFlow("Perfume")
+    val type get() = _type.asStateFlow()
 
+    //현재 page
+    private val _page = MutableStateFlow(0)
+    val page get() = _page.asStateFlow()
+
+    //comment 리스트
+    private val _posts = MutableStateFlow(emptyList<CommunityByCategoryResponseDto>())
+    val posts get() = _posts.asStateFlow()
+
+    val uiState: StateFlow<PostUiState> = combine(
+        type,
+        posts,
+        PostUiState::Posts
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(3_000),
+        initialValue = PostUiState.Loading
+    )
+
+    //page 증가
+    fun addPage(){
+        _page.update{_page.value + 1}
+        updatePosts()
+    }
+
+    //type 변환
+    fun changeType(newType : String){
+        if (_type.value != newType) {
+            _type.update{newType}
+        }
+        updatePosts()
+    }
+
+    //comment list 업데이트
+    private fun updatePosts(){
+        viewModelScope.launch(Dispatchers.IO){
+            postUseCase(page.value).map{
+                _posts.update{it}
+            }
+        }
+    }
+
+}
+
+sealed interface PostUiState {
+    data object Loading : PostUiState
+
+    data class Posts(
+        val type : String,
+        val posts : List<CommunityByCategoryResponseDto>
+    ) : PostUiState
+
+    data object Empty : PostUiState
 }

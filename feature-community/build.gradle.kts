8808e6efd44179bd64b167299a3860b7ee4091cb
uselessNaming <seohojon@naver.com>
2024-03-13 18:04:59 +0900

    Chore: Compose Option 설정

--- feature-community/build.gradle.kts
+++ feature-community/build.gradle.kts
@@ -31,6 +31,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {

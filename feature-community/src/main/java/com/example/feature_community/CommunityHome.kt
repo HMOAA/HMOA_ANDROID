6f04642b17797b15516f3c0cfc5c87ef3502104f
uselessNaming <seohojon@naver.com>
2024-03-14 13:50:55 +0900

    Feat: Floating Button 추가

--- feature-community/src/main/java/com/example/feature_community/CommunityHome.kt
+++ feature-community/src/main/java/com/example/feature_community/CommunityHome.kt
@@ -1,7 +1,11 @@
 package com.example.feature_community
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +17,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hmoa.component.PostListItem
+import com.hmoa.core_model.response.CommunityByCategoryResponseDto
+import com.hmoa.core_model.response.CommunityCommentWithLikedResponseDto
 import com.hmoa.core_model.response.CommunityDefaultResponseDto
 
 @Composable
@@ -36,49 +43,49 @@ fun CommunityHomeRoute(){
 
 @Composable
 fun CommunityHome(
-    communities : List<CommunityDefaultResponseDto>, //이거 uiState로 이전해서 uiState에서 데이터 가져오는 방식으로
+    communities : List<CommunityByCategoryResponseDto>, //이거 uiState로 이전해서 uiState에서 데이터 가져오는 방식으로
     onNavCommunityByCategory : () -> Unit, //카테고리 별 Community 화면으로 이동
     onNavCommunityDescription : () -> Unit, //해당 Community Id를 가진 Description 화면으로 이동
 ){
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize()
             .padding(horizontal = 16.dp)
     ){
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Bottom
         ){
             Text(
                 text = "Community",
                 fontSize = 16.sp,
                 color = Color.Black
             )
-            
-            TextButton(
-                onClick = {
-                    /** 여기서 카테고리 별 조회 화면 (real community home)으로 이동 */
+
+            Text(
+                modifier = Modifier.clickable{
                     onNavCommunityByCategory()
                 },
-            ){
-                Text(
-                    text = "전체보기",
-                    fontSize = 12.sp,
-                    color = Color.Black
-                )
-            }
+                text = "전체보기",
+                fontSize = 12.sp,
+                color = Color.Black
+            )
         }
 
         Spacer(Modifier.height(16.dp))
 
-        LazyColumn{
+        LazyColumn(
+            modifier = Modifier.background(color = Color.White),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
             items(communities){community ->
                 PostListItem(
                     onPostClick = {
                         /** 여기서 해당 post description으로 이동 */
                         onNavCommunityDescription()
                     },
-                    postType = "",
-                    postTitle = ""
+                    postType = community.category,
+                    postTitle = community.title
                 )
             }
         }
@@ -88,8 +95,26 @@ fun CommunityHome(
 @Preview(showBackground = true)
 @Composable
 fun TestCommunityHome(){
+    val testList = listOf(
+        CommunityByCategoryResponseDto(
+            category = "시향기",
+            commentCount = 0,
+            communityId = 0,
+            heartCount = 10,
+            liked = true,
+            title = "여자친구한테 선물할 향수 뭐가 좋을까요?"
+        ),
+        CommunityByCategoryResponseDto(
+            category = "시향기",
+            commentCount = 0,
+            communityId = 0,
+            heartCount = 10,
+            liked = true,
+            title = "여자친구한테 선물할 향수 뭐가 좋을까요?"
+        )
+    )
     CommunityHome(
-        communities = emptyList(),
+        communities = testList,
         onNavCommunityDescription = {},
         onNavCommunityByCategory = {}
     )

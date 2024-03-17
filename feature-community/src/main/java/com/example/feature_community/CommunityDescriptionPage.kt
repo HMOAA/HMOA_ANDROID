357924b734dfeaf5a4a4e0dcccda96b5c8106a15
uselessNaming <seohojon@naver.com>
2024-03-17 22:46:18 +0900

    Feat: 파라미터 수정

--- feature-community/src/main/java/com/example/feature_community/CommunityDescriptionPage.kt
+++ feature-community/src/main/java/com/example/feature_community/CommunityDescriptionPage.kt
@@ -2,6 +2,7 @@ package com.example.feature_community
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
@@ -14,7 +15,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,9 +53,11 @@ fun CommunityDescriptionRoute(
 //    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommunityDescriptionPage(
     //bottom sheet 띄우는 state 갱신 event
+    isOpenBottomOptions : Boolean,
     changeBottomOptionState : (Boolean) -> Unit,
     category : Category,
     profile : String,
@@ -60,12 +67,17 @@ fun CommunityDescriptionPage(
     content : String,
     heartCount : String,
     isLiked : Boolean,
+    isWritten : Boolean,
     commentList : List<CommunityCommentDefaultResponseDto> = listOf(),
+    onClickReport : () -> Unit,
+    onDeleteCommunity : () -> Unit,
     onNavBack : () -> Unit,
+    onNavCommunityEdit : () -> Unit,
 ){
 
     val scrollState = rememberScrollState()
 
+    /** Text Style 정의 */
     val categoryTextStyle = TextStyle(
         fontSize = 14.sp,
         color = CustomColor.gray2
@@ -78,7 +90,98 @@ fun CommunityDescriptionPage(
         fontSize = 12.sp,
         color = Color.Black
     )
+    val noDataTextStyle = TextStyle(
+        fontSize = 20.sp,
+        color = Color.Black
+    )
+    val dialogDefaultTextStyle = TextStyle(
+        fontSize = 20.sp,
+        color = CustomColor.blue
+    )
+    val dialogRedTextStyle = TextStyle(
+        fontSize = 20.sp,
+        color = CustomColor.red
+    )
+    
+    if (isOpenBottomOptions) {
+        ModalBottomSheet(
+            onDismissRequest = { changeBottomOptionState(false) },
+            containerColor = Color.White,
+            scrimColor = Color.Black.copy(alpha = 0.3f),
+        ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.Transparent, shape = RoundedCornerShape(size = 20.dp))
+                    .padding(vertical = 16.dp)
+            ) {
+                if (isWritten) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                /** 게시글 수정 화면으로 이동 */
+                                onNavCommunityEdit()
+                            },
+                        text = "수정",
+                        textAlign = TextAlign.Center,
+                        style = dialogDefaultTextStyle
+                    )
 
+                    Spacer(Modifier.height(16.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                /** 커뮤니티 삭제 */
+                                onDeleteCommunity()
+                            },
+                        text = "삭제",
+                        textAlign = TextAlign.Center,
+                        style = dialogDefaultTextStyle
+                    )
+
+                    Spacer(Modifier.height(16.dp))
+
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                changeBottomOptionState(false)
+                            },
+                        text = "취소",
+                        textAlign = TextAlign.Center,
+                        style = dialogRedTextStyle
+                    )
+
+                } else {
+                    Text(
+                        modifier = Modifier.fillMaxWidth()
+                            .clickable{
+                                /** 신고 이벤트 */
+                                onClickReport()
+                            },
+                        text = "신고하기",
+                        textAlign = TextAlign.Center,
+                        style = dialogDefaultTextStyle
+                    )
+
+                    Spacer(Modifier.height(16.dp))
+
+                    Text(
+                        modifier = Modifier.fillMaxWidth()
+                            .clickable{
+                                changeBottomOptionState(false)
+                            },
+                        text = "취소",
+                        textAlign = TextAlign.Center,
+                        style = dialogRedTextStyle
+                    )
+                }
+            }
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -155,20 +258,29 @@ fun CommunityDescriptionPage(
 
             Spacer(Modifier.height(21.dp))
 
-            commentList.forEachIndexed{ index, comment ->
-                Comment(
-                    profile = comment.profileImg,
-                    nickname = comment.nickname,
-                    dateDiff = comment.createAt,
-                    comment = comment.content,
-                    isFirst = false,
-                    viewNumber = if (comment.heartCount > 999) "999+" else comment.heartCount.toString(),
-                    onNavCommunity = {}
-                )
+            if (commentList.isNotEmpty()){
+                commentList.forEachIndexed{ index, comment ->
+                    Comment(
+                        profile = comment.profileImg,
+                        nickname = comment.nickname,
+                        dateDiff = comment.createAt,
+                        comment = comment.content,
+                        isFirst = false,
+                        viewNumber = if (comment.heartCount > 999) "999+" else comment.heartCount.toString(),
+                        onNavCommunity = {}
+                    )
 
-                if (index != commentList.size - 1) {
-                    Spacer(Modifier.height(15.dp))
+                    if (index != commentList.size - 1) {
+                        Spacer(Modifier.height(15.dp))
+                    }
                 }
+            } else {
+                Spacer(Modifier.height(40.dp))
+
+                Text(
+                    text = "아직 작성한 댓글이 없습니다",
+                    style = noDataTextStyle
+                )
             }
         }
     }
@@ -181,6 +293,7 @@ fun CommunityDescriptionPage(
 fun TestCommunityDescriptionPage(){
     var isOpenBottomOptions by remember{mutableStateOf(false)}
     CommunityDescriptionPage(
+        isOpenBottomOptions = isOpenBottomOptions,
         changeBottomOptionState = {
             isOpenBottomOptions = it
         },
@@ -192,6 +305,10 @@ fun TestCommunityDescriptionPage(){
         content = "테스트 내용입니다. 테스트 내용입니다. 테스트 내용입니다. 테스트 내용입니다. 테스트 내용입니다. 테스트 내용입니다. 테스트 내용입니다.",
         heartCount = "259",
         isLiked = false,
-        onNavBack = {}
+        isWritten = false,
+        onNavBack = {},
+        onClickReport = {},
+        onDeleteCommunity = {},
+        onNavCommunityEdit = {}
     )
 }

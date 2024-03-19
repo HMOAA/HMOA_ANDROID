9080930921d96c75332066ced3d52ddab8cffa2e
uselessNaming <seohojon@naver.com>
2024-03-19 15:37:35 +0900

    Feat: View Model과 Route 연결

--- feature-community/src/main/java/com/example/feature_community/CommunityPage.kt
+++ feature-community/src/main/java/com/example/feature_community/CommunityPage.kt
@@ -26,6 +26,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.feature_community.ViewModel.CommunityMainUiState
+import com.example.feature_community.ViewModel.CommunityMainViewModel
 import com.hmoa.component.PostListItem
 import com.hmoa.component.TopBar
 import com.hmoa.core_designsystem.component.FloatingActionBtn
@@ -38,140 +42,153 @@ import com.hmoa.feature_community.R
 @Composable
 fun CommunityPageRoute(
     onNavBack : () -> Unit,
-    onNavCommunityDescription : () -> Unit,
+    onNavCommunityDescription : (Int) -> Unit,
+    onNavPost : (String) -> Unit,
+    viewModel : CommunityMainViewModel = hiltViewModel()
 ){
 
     /** 선택된 type을 Page에 넘기고 이에 따라 isSelect 값을 선택 */
     //view model의 ui state에서 type, list 를 받아서 사용하는 방식
-
-    var type = Category.추천
-    val list = listOf<CommunityByCategoryResponseDto>()
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
 
     CommunityPage(
-        selectedType = type,
+        uiState = uiState.value,
         onTypeChanged = {
-            type = it
+            viewModel.updateCategory(it)
         },
-        communities = list,
         onNavBack = onNavBack,
         onNavCommunityDescription = onNavCommunityDescription,
-        onNavRecommend = {},
-        onNavPresent = {},
-        onNavFree = {}
+        onNavPost = onNavPost
     )
 }
 
 @Composable
 fun CommunityPage(
-    selectedType : Category,
+    uiState : CommunityMainUiState,
     onTypeChanged : (Category) -> Unit,
-    communities : List<CommunityByCategoryResponseDto>,
     onNavBack : () -> Unit,
-    onNavCommunityDescription: () -> Unit,
-    onNavRecommend : () -> Unit,
-    onNavPresent : () -> Unit,
-    onNavFree : () -> Unit,
+    onNavCommunityDescription: (Int) -> Unit,
+    onNavPost : (String) -> Unit,
 ){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomEnd
-    ){
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ){
-            TopBar(
-                title = "Community",
-                navIcon = painterResource(com.hmoa.core_designsystem.R.drawable.ic_back),
-                onNavClick = onNavBack,
-            )
+    when(uiState) {
+        is CommunityMainUiState.Loading -> {
 
-            Divider(
-                Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(color = CustomColor.gray2)
-            )
-
-            /** search bar (여기서는 Search 모듈을 사용해야 하지 않을까?) */
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp)
-            )
-
-            Divider(
-                Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(color = CustomColor.gray2)
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(44.dp)
-                    .padding(start = 32.dp),
-                verticalAlignment = Alignment.CenterVertically
+        }
+        is CommunityMainUiState.Community -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
             ){
-                TypeBadge(
-                    roundedCorner = 20.dp,
-                    type = Category.추천.name,
-                    fontSize = 14.sp,
-                    fontColor = Color.White,
-                    selected = selectedType == Category.추천
-                )
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    TopBar(
+                        title = "Community",
+                        navIcon = painterResource(com.hmoa.core_designsystem.R.drawable.ic_back),
+                        onNavClick = onNavBack,
+                    )
 
-                Spacer(Modifier.width(8.dp))
+                    Divider(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(color = CustomColor.gray2)
+                    )
 
-                TypeBadge(
-                    roundedCorner = 20.dp,
-                    type = Category.시향기.name,
-                    fontSize = 14.sp,
-                    fontColor = Color.White,
-                    selected = selectedType == Category.시향기
-                )
-
-                Spacer(Modifier.width(8.dp))
-
-                TypeBadge(
-                    roundedCorner = 20.dp,
-                    type = Category.자유.name,
-                    fontSize = 14.sp,
-                    fontColor = Color.White,
-                    selected = selectedType == Category.자유
-                )
-            }
-
-            Divider(
-                Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(color = CustomColor.gray2)
-            )
-
-            LazyColumn{
-                items(communities){community ->
-                    PostListItem(
+                    /** search bar (여기서는 Search 모듈을 사용해야 하지 않을까?) */
+                    Spacer(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .wrapContentHeight()
-                            .border(width = 1.dp, color = CustomColor.gray2),
-                        onPostClick = {
-                            /** 여기서 Description으로 이동 */
-                            /** id를 어떤 방식으로 전달할지 */
-                            onNavCommunityDescription()
-                        },
-                        postType = community.category,
-                        postTitle =community.title,
+                            .height(60.dp)
                     )
+
+                    Divider(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(color = CustomColor.gray2)
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .padding(start = 32.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        TypeBadge(
+                            onClickItem = {
+                                onTypeChanged(Category.추천)
+                            },
+                            roundedCorner = 20.dp,
+                            type = Category.추천.name,
+                            fontSize = 14.sp,
+                            fontColor = Color.White,
+                            selected = uiState.type == Category.추천
+                        )
+
+                        Spacer(Modifier.width(8.dp))
+
+                        TypeBadge(
+                            onClickItem = {
+                                onTypeChanged(Category.시향기)
+                            },
+                            roundedCorner = 20.dp,
+                            type = Category.시향기.name,
+                            fontSize = 14.sp,
+                            fontColor = Color.White,
+                            selected = uiState.type == Category.시향기
+                        )
+
+                        Spacer(Modifier.width(8.dp))
+
+                        TypeBadge(
+                            onClickItem = {
+                                onTypeChanged(Category.자유)
+                            },
+                            roundedCorner = 20.dp,
+                            type = Category.자유.name,
+                            fontSize = 14.sp,
+                            fontColor = Color.White,
+                            selected = uiState.type == Category.자유
+                        )
+                    }
+
+                    Divider(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(color = CustomColor.gray2)
+                    )
+
+                    LazyColumn{
+                        items(uiState.communities){community ->
+                            PostListItem(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight()
+                                    .border(width = 1.dp, color = CustomColor.gray2),
+                                onPostClick = {
+                                    /** 여기서 Description으로 이동 */
+                                    /** id를 어떤 방식으로 전달할지 */
+                                    onNavCommunityDescription(community.communityId)
+                                },
+                                postType = community.category,
+                                postTitle =community.title,
+                            )
+                        }
+                    }
                 }
+                FloatingActionBtn(
+                    onNavRecommend = { onNavPost(Category.추천.name) },
+                    onNavPresent = { onNavPost(Category.시향기.name) },
+                    onNavFree = { onNavPost(Category.자유.name) },
+                )
             }
         }
-        FloatingActionBtn(
-            onNavRecommend = onNavRecommend,
-            onNavPresent = onNavPresent,
-            onNavFree = onNavFree,
-        )
+        is CommunityMainUiState.Empty -> {
+
+        }
     }
 }
 
@@ -198,15 +215,12 @@ fun TestCommunity(){
         )
     )
     CommunityPage(
-        selectedType = type,
+        uiState = CommunityMainUiState.Loading,
         onTypeChanged = {
             type = it
         },
-        communities = testList,
         onNavBack = {},
         onNavCommunityDescription = {},
-        onNavRecommend = {},
-        onNavPresent = {},
-        onNavFree = {}
+        onNavPost = {},
     )
 }

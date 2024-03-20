89acb3b2e8017e84271b84474b328aab4e4e43bd
uselessNaming <seohojon@naver.com>
2024-03-21 08:40:55 +0900

    Comment: Log 실행 확인

--- app/src/main/java/com/hmoa/app/App.kt
+++ app/src/main/java/com/hmoa/app/App.kt
@@ -1,6 +1,7 @@
 package com.hmoa.app
 
 import android.app.Application
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -8,6 +9,7 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
+        Log.d("A123", "App : Application - onCreate()")
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
--- app/src/main/java/com/hmoa/app/AppViewModel.kt
+++ app/src/main/java/com/hmoa/app/AppViewModel.kt
@@ -1,5 +1,6 @@
 package com.hmoa.app
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.hmoa.app.navigation.MAIN_ROUTE
 import com.hmoa.core_domain.usecase.GetAuthAndRememberedTokenUseCase
@@ -11,13 +12,16 @@ import javax.inject.Inject
 class AppViewModel @Inject constructor(private val getAuthAndRememberedTokenUseCase: GetAuthAndRememberedTokenUseCase) :
     ViewModel() {
     suspend fun routeInitialScreen(): String {
+        Log.d("A123","routeInit")
         val pair = getAuthAndRememberedTokenUseCase()
         val authToken = pair.first
         val rememberedToken = pair.second
 
+        Log.d("A123","token check")
         if (authToken == null && rememberedToken == null) {
             return LOGIN_ROUTE
         }
+        Log.d("A123", "return")
         return MAIN_ROUTE
     }
 }

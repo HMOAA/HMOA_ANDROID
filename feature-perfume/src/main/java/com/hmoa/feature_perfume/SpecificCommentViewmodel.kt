814748bc6b54e054655ae19791597391e14f30b1
이용인 <yongin0517@gmail.com>
2024-03-22 01:12:46 +0900

    Fix: Viewmodel 상속 누락 수정

--- feature-perfume/src/main/java/com/hmoa/feature_perfume/SpecificCommentViewmodel.kt
+++ feature-perfume/src/main/java/com/hmoa/feature_perfume/SpecificCommentViewmodel.kt
@@ -1,17 +1,15 @@
 package com.hmoa.feature_perfume
 
+import androidx.lifecycle.ViewModel
 import com.hmoa.core_domain.repository.PerfumeCommentRepository
-import com.hmoa.core_model.response.PerfumeCommentGetResponseDto
 import com.hmoa.core_model.response.PerfumeCommentResponseDto
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 @HiltViewModel
 class SpecificCommentViewmodel @Inject constructor(
     private val perfumeCommentRepository: PerfumeCommentRepository
-) {
+) : ViewModel() {
 
     sealed interface SpecificCommentUiState {
         data object Loading : SpecificCommentUiState

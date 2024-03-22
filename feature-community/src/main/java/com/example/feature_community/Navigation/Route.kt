a2e104fc3dfc7e4ac66c74d483f2fa0460646f50
uselessNaming <seohojon@naver.com>
2024-03-22 22:46:09 +0900

    Rename: 패키지 이름 변경 (example -> hmoa)

--- feature-community/src/main/java/com/example/feature_community/Navigation/Route.kt
+++
@@ -1,9 +0,0 @@
-package com.example.feature_community.Navigation
-
-enum class Route {
-    CommunityDescriptionRoute,
-    CommunityEditRoute,
-    CommunityHomeRoute,
-    CommunityPageRoute,
-    CommunityPostRoute,
-}

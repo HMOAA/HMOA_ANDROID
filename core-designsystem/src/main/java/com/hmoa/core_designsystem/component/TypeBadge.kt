661f705699b4a962de2534bf0cf34bb3877ab498
uselessNaming <seohojon@naver.com>
2024-03-25 20:52:39 +0900

    Design: type bedge click 이벤트 효과와 배경 크기 맞춤

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/TypeBadge.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/TypeBadge.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.TextStyle
@@ -44,6 +45,7 @@ fun TypeBadge(
     Row(
         modifier = Modifier
             .background(color = backgroundColor, shape = RoundedCornerShape(size = roundedCorner))
+            .clip(RoundedCornerShape(size = roundedCorner))
             .clickable{
                 onClickItem()
             }

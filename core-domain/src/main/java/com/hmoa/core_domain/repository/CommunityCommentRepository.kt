59d78b35badd47a4773ab0b351a5064347f843f0
uselessNaming <seohojon@naver.com>
2024-03-26 10:47:43 +0900

    Feat: Repository Result Response로 Wrapping

--- core-domain/src/main/java/com/hmoa/core_domain/repository/CommunityCommentRepository.kt
+++ core-domain/src/main/java/com/hmoa/core_domain/repository/CommunityCommentRepository.kt
@@ -1,5 +1,6 @@
 package com.hmoa.core_domain.repository
 
+import ResultResponse
 import com.hmoa.core_model.request.CommunityCommentDefaultRequestDto
 import com.hmoa.core_model.response.CommunityCommentAllResponseDto
 import com.hmoa.core_model.response.CommunityCommentDefaultResponseDto
@@ -10,19 +11,19 @@ interface CommunityCommentRepository {
     suspend fun putCommunityComment(
         commentId: Int,
         dto: CommunityCommentDefaultRequestDto
-    ): CommunityCommentWithLikedResponseDto
+    ): ResultResponse<CommunityCommentWithLikedResponseDto>
 
-    suspend fun deleteCommunityComment(commentId: Int): DataResponseDto<Any>
+    suspend fun deleteCommunityComment(commentId: Int): ResultResponse<DataResponseDto<Any>>
 
     suspend fun putCommunityCommentLiked(
         commentId: Int,
         dto: CommunityCommentDefaultRequestDto
-    ): DataResponseDto<Any>
+    ): ResultResponse<DataResponseDto<Any>>
 
-    suspend fun deleteCommunityCommentLiked(commentId: Int): DataResponseDto<Any>
-    suspend fun getCommunityComments(communityId: Int, page: Int): CommunityCommentAllResponseDto
+    suspend fun deleteCommunityCommentLiked(commentId: Int): ResultResponse<DataResponseDto<Any>>
+    suspend fun getCommunityComments(communityId: Int, page: Int): ResultResponse<CommunityCommentAllResponseDto>
     suspend fun postCommunityComment(
         communityId: Int,
         dto: CommunityCommentDefaultRequestDto
-    ): CommunityCommentWithLikedResponseDto
+    ): ResultResponse<CommunityCommentWithLikedResponseDto>
 }

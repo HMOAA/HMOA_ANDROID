9ea63e3251471b3e9b6fde25db26fd8c3e14b5ed
이용인 <yongin0517@gmail.com>
2024-03-27 07:00:42 +0900

    Test: ResultResponse객체 테스트 생성

--- feature-authentication/src/test/java/com/hmoa/feature_authentication/ExampleUnitTest.kt
+++ feature-authentication/src/test/java/com/hmoa/feature_authentication/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.hmoa.feature_authentication
 
+import com.hmoa.core_common.Result
+import com.hmoa.core_common.asResult
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    //언제 Result.Error가 언제 나오는지 테스트
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    suspend fun ResultResponseErrorTest() {
+        val exceptionCases = listOf<Any>("good", "nice", Exception("500"))
+        flow {
+            exceptionCases.map { emit(it) }
+        }.asResult().collectLatest {
+            when (it) {
+                is Result.Success -> {
+
+                }
+
+                is Result.Loading -> {
+
+                }
+
+                is Result.Error -> {
+
+                }
+            }
+        }
     }
 }

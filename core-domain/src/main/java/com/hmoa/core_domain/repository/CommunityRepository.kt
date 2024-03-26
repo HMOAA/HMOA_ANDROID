9d0b17514dfc15d80e6a8dad9b8ea90fdbb4f98f
uselessNaming <seohojon@naver.com>
2024-03-26 10:52:35 +0900

    Feat: Repository Result Response로 Wrapping

--- core-domain/src/main/java/com/hmoa/core_domain/repository/CommunityRepository.kt
+++ core-domain/src/main/java/com/hmoa/core_domain/repository/CommunityRepository.kt
@@ -1,5 +1,6 @@
 package com.hmoa.core_domain.repository
 
+import ResultResponse
 import com.hmoa.core_model.Category
 import com.hmoa.core_model.response.CommunityByCategoryResponseDto
 import com.hmoa.core_model.response.CommunityDefaultResponseDto
@@ -7,24 +8,24 @@ import com.hmoa.core_model.response.DataResponseDto
 import java.io.File
 
 interface CommunityRepository {
-    suspend fun getCommunity(communityId: Int): CommunityDefaultResponseDto
+    suspend fun getCommunity(communityId: Int): ResultResponse<CommunityDefaultResponseDto>
     suspend fun postCommunityUpdate(
         images: Array<File>,
         deleteCommunityPhotoIds: Array<Int>,
         title: String,
         content: String,
         communityId: Int
-    ): CommunityDefaultResponseDto
+    ): ResultResponse<CommunityDefaultResponseDto>
 
-    suspend fun deleteCommunity(communityId: Int): DataResponseDto<Nothing>
-    suspend fun putCommunityLike(communityId : Int) : DataResponseDto<Nothing>
-    suspend fun deleteCommunityLike(communityId: Int) : DataResponseDto<Nothing>
-    suspend fun getCommunityByCategory(category: String, page: Int): List<CommunityByCategoryResponseDto>
-    suspend fun getCommunitiesHome(): List<CommunityByCategoryResponseDto>
+    suspend fun deleteCommunity(communityId: Int): ResultResponse<DataResponseDto<Nothing>>
+    suspend fun putCommunityLike(communityId : Int) : ResultResponse<DataResponseDto<Nothing>>
+    suspend fun deleteCommunityLike(communityId: Int) : ResultResponse<DataResponseDto<Nothing>>
+    suspend fun getCommunityByCategory(category: String, page: Int): ResultResponse<List<CommunityByCategoryResponseDto>>
+    suspend fun getCommunitiesHome(): ResultResponse<List<CommunityByCategoryResponseDto>>
     suspend fun postCommunitySave(
         images: Array<File>,
         category: String,
         title: String,
         content: String
-    ): CommunityDefaultResponseDto
+    ): ResultResponse<CommunityDefaultResponseDto>
 }
--- core-repository/src/main/java/com/hmoa/core_repository/CommunityRepositoryImpl.kt
+++ core-repository/src/main/java/com/hmoa/core_repository/CommunityRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.hmoa.core_repository
 
+import ResultResponse
 import com.hmoa.core_datastore.Community.CommunityDataStore
 import com.hmoa.core_model.Category
 import com.hmoa.core_model.response.CommunityByCategoryResponseDto
@@ -10,7 +11,7 @@ import javax.inject.Inject
 
 class CommunityRepositoryImpl @Inject constructor(private val communityDataStore: CommunityDataStore) :
     com.hmoa.core_domain.repository.CommunityRepository {
-    override suspend fun getCommunity(communityId: Int): CommunityDefaultResponseDto {
+    override suspend fun getCommunity(communityId: Int): ResultResponse<CommunityDefaultResponseDto> {
         return communityDataStore.getCommunity(communityId)
     }
 
@@ -20,30 +21,30 @@ class CommunityRepositoryImpl @Inject constructor(private val communityDataStore
         title: String,
         content: String,
         communityId: Int
-    ): CommunityDefaultResponseDto {
+    ): ResultResponse<CommunityDefaultResponseDto> {
         return communityDataStore.postCommunityUpdate(images, deleteCommunityPhotoIds, title, content, communityId)
     }
 
-    override suspend fun deleteCommunity(communityId: Int): DataResponseDto<Nothing> {
+    override suspend fun deleteCommunity(communityId: Int): ResultResponse<DataResponseDto<Nothing>> {
         return communityDataStore.deleteCommunity(communityId)
     }
 
-    override suspend fun putCommunityLike(communityId: Int): DataResponseDto<Nothing> {
+    override suspend fun putCommunityLike(communityId: Int): ResultResponse<DataResponseDto<Nothing>> {
         return communityDataStore.putCommunityLike(communityId)
     }
 
-    override suspend fun deleteCommunityLike(communityId: Int): DataResponseDto<Nothing> {
+    override suspend fun deleteCommunityLike(communityId: Int): ResultResponse<DataResponseDto<Nothing>> {
         return communityDataStore.deleteCommunity(communityId)
     }
 
     override suspend fun getCommunityByCategory(
         category: String,
         page: Int
-    ): List<CommunityByCategoryResponseDto> {
+    ): ResultResponse<List<CommunityByCategoryResponseDto>> {
         return communityDataStore.getCommunityByCategory(category, page)
     }
 
-    override suspend fun getCommunitiesHome(): List<CommunityByCategoryResponseDto> {
+    override suspend fun getCommunitiesHome(): ResultResponse<List<CommunityByCategoryResponseDto>> {
         return communityDataStore.getCommunitiesHome()
     }
 
@@ -52,7 +53,7 @@ class CommunityRepositoryImpl @Inject constructor(private val communityDataStore
         category: String,
         title: String,
         content: String
-    ): CommunityDefaultResponseDto {
+    ): ResultResponse<CommunityDefaultResponseDto> {
         return communityDataStore.postCommunitySave(images, category, title, content)
     }
 }

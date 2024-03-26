5351c95a0a0cc9110c7f7c062ba765b5745c2be7
이용인 <yongin0517@gmail.com>
2024-03-26 15:18:42 +0900

    Chore: feature-home gradle의존성 추가

--- feature-home/build.gradle.kts
+++ feature-home/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -27,14 +30,43 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
 
+    val hilt_version = "2.44"
+    val hilt_viewmodel_version = "1.0.0-alpha03"
+    val hilt_nav_compose_version = "1.0.0"
+
+    implementation(project(":core-designsystem"))
+    implementation(project(":core-domain"))
+    implementation(project(":core-model"))
+    implementation(project(":core-common"))
+
+    implementation("androidx.compose.material:material:1.2.0-beta02")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.paging:paging-compose:1.0.0-alpha02")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    implementation("com.google.dagger:hilt-compiler:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+    testAnnotationProcessor("com.google.dagger:hilt-compiler:$hilt_version")
+    implementation("androidx.hilt:hilt-navigation-compose:$hilt_nav_compose_version")
+    kapt("androidx.hilt:hilt-compiler:$hilt_viewmodel_version")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
 }

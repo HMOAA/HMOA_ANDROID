cc03631e05c2da1a3b26e677c044132902c8a535
이용인 <yongin0517@gmail.com>
2024-03-31 00:16:05 +0900

    Docs: 컴파일 오류 임시 주석처리

--- feature-community/src/main/java/com/hmoa/feature_community/ViewModel/CommunityDescViewModel.kt
+++ feature-community/src/main/java/com/hmoa/feature_community/ViewModel/CommunityDescViewModel.kt
@@ -12,25 +12,15 @@ import com.hmoa.core_model.request.CommunityCommentDefaultRequestDto
 import com.hmoa.core_model.response.CommunityCommentAllResponseDto
 import com.hmoa.core_model.response.CommunityDefaultResponseDto
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CommunityDescViewModel @Inject constructor(
-    private val communityRepository : CommunityRepository,
-    private val communityCommentRepository : CommunityCommentRepository,
-    getUserInfo : GetMyUserInfoUseCase,
+    private val communityRepository: CommunityRepository,
+    private val communityCommentRepository: CommunityCommentRepository,
+    getUserInfo: GetMyUserInfoUseCase,
 ) : ViewModel() {
 
     private val _isOpenBottomOptions = MutableStateFlow(false)
@@ -54,40 +44,38 @@ class CommunityDescViewModel @Inject constructor(
     private val _profile = MutableStateFlow<String?>(null)
     val profile get() = _profile.asStateFlow()
 
-    init{
-        viewModelScope.launch{
+    init {
+        viewModelScope.launch {
             Log.d("TAG TEST", "view model init")
             getUserInfo().asResult()
-                .map{ result ->
-                    when(result) {
-                        Result.Loading -> _profile.update{null}
+                .map { result ->
+                    when (result) {
+                        Result.Loading -> _profile.update { null }
                         is Result.Success -> {
                             _profile.update { result.data.profile }
                         }
-                        is Result.Error -> _errState.update{ result.exception.toString() }
+
+                        is Result.Error -> _errState.update { result.exception.toString() }
                     }
                 }
         }
     }
 
-    val uiState : StateFlow<CommunityDescUiState> = combine(
-        flow{
+    val uiState: StateFlow<CommunityDescUiState> = combine(
+        flow {
             val result = communityRepository.getCommunity(id.value)
-            if (result.data == null) {
-                _errState.update { "${result.errorCode} : ${result.errorMessage}"}
-                return@flow
-            }
+
             emit(result.data!!)
         }.asResult(),
-        flow{
+        flow {
             val result = communityCommentRepository.getCommunityComments(id.value, page.value)
             if (result.data == null) {
-                _errState.update { "${result.errorCode} : ${result.errorMessage}"}
+                //_errState.update { "${result.errorCode} : ${result.errorMessage}"}
                 return@flow
             }
             emit(result.data!!)
         }.asResult()
-    ){ communityResult, commentsResult ->
+    ) { communityResult, commentsResult ->
         when {
             communityResult is Result.Error || commentsResult is Result.Error -> CommunityDescUiState.Error
             communityResult is Result.Loading || commentsResult is Result.Loading -> CommunityDescUiState.Loading
@@ -105,29 +93,29 @@ class CommunityDescViewModel @Inject constructor(
     )
 
     //bottom option 상태 업데이트
-    fun updateBottomOptionsState(state : Boolean){
-        _isOpenBottomOptions.update{ state }
+    fun updateBottomOptionsState(state: Boolean) {
+        _isOpenBottomOptions.update { state }
     }
 
     //신고하기
-    fun reportCommunity(){
+    fun reportCommunity() {
         /** 신고하기 repository 추가되어서 이거 추가해야 함 */
     }
 
     //삭제
-    fun delCommunity(){
-        viewModelScope.launch{
-            try{
+    fun delCommunity() {
+        viewModelScope.launch {
+            try {
                 communityRepository.deleteCommunity(id.value)
-            } catch (e : Exception) {
-                _errState.update{ e.message.toString() }
+            } catch (e: Exception) {
+                _errState.update { e.message.toString() }
             }
         }
     }
 
     //댓글 작성
-    fun postComment(comment : String){
-        viewModelScope.launch{
+    fun postComment(comment: String) {
+        viewModelScope.launch {
             val requestDto = CommunityCommentDefaultRequestDto(
                 content = comment
             )
@@ -137,35 +125,37 @@ class CommunityDescViewModel @Inject constructor(
                     communityId = id.value,
                     dto = requestDto
                 )
-            } catch (e : Exception) {
-                _errState.update{ e.message.toString() }
+            } catch (e: Exception) {
+                _errState.update { e.message.toString() }
             }
         }
     }
 
     //id 설정
     fun setId(id: Int) {
-        _id.update{ id }
+        _id.update { id }
     }
 }
 
-sealed interface CommunityDescUiState{
+sealed interface CommunityDescUiState {
     data object Loading : CommunityDescUiState
     data class CommunityDesc(
-        val community : CommunityDefaultResponseDto,
-        val comments : CommunityCommentAllResponseDto
+        val community: CommunityDefaultResponseDto,
+        val comments: CommunityCommentAllResponseDto
     ) : CommunityDescUiState {
-        val photoList = community.communityPhotos.map{
+        val photoList = community.communityPhotos.map {
             it.photoUrl
         }
     }
+
     data object Error : CommunityDescUiState
 }
 
-sealed interface UserInfoState{
+sealed interface UserInfoState {
     data object Loading : UserInfoState
     data class User(
-        val profile : String,
+        val profile: String,
     ) : UserInfoState
+
     data object Error : UserInfoState
 }

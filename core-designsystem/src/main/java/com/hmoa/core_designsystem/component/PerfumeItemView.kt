101e0aa493105824fc38c9d70383966cfc7896df
이용인 <yongin0517@gmail.com>
2024-03-31 15:30:10 +0900

    Fix: ImageView 사이즈 조정 매개변수 변경

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/PerfumeItemView.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/PerfumeItemView.kt
@@ -1,24 +1,47 @@
 package com.hmoa.core_designsystem.component
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun PerfumeItemView(imageUrl: String, perfumeName: String, brandName: String, width: Int, height: Int, imageBackgroundColor:Color) {
-    Column(modifier = Modifier.padding(end = 8.dp).width(88.dp)) {
-        ImageView(imageUrl = imageUrl, backgroundColor = imageBackgroundColor, width = width, height = height)
+fun PerfumeItemView(
+    imageUrl: String,
+    perfumeName: String,
+    brandName: String,
+    containerWidth: Int,
+    containerHeight: Int,
+    imageWidth: Float,
+    imageHeight: Float,
+    imageBackgroundColor: Color
+) {
+    Column(modifier = Modifier.padding(end = 8.dp).width(containerWidth.dp)) {
+        Column(
+            modifier = Modifier.width(containerWidth.dp).height(containerHeight.dp)
+                .background(imageBackgroundColor),
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            ImageView(
+                imageUrl = imageUrl,
+                backgroundColor = imageBackgroundColor,
+                width = imageWidth,
+                height = imageHeight,
+                contentScale = ContentScale.Fit
+            )
+        }
         Text(
             text = brandName, style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Medium),
-            modifier = Modifier.padding(end = 4.dp)
+            modifier = Modifier.padding(end = 4.dp, top = 8.dp)
         )
         Text(
             text = perfumeName, style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Medium),

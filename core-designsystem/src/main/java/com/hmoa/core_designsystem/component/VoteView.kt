4bd656822897d7b38396f2748fcdfa7635b6e72e
이용인 <yongin0517@gmail.com>
2024-04-03 17:58:47 +0900

    Design: 글씨 및 아이콘 색깔 변경

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/VoteView.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/VoteView.kt
@@ -4,17 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -26,52 +22,62 @@ import com.hmoa.core_designsystem.theme.CustomColor
 
 @Composable
 fun VoteView(
-    percentage : Int,
-    icon : Painter,
-    title:String,
-    onVote:() -> Unit
-){
+    percentage: Int,
+    icon: Painter,
+    title: String,
+    onVote: () -> Unit
+) {
     val percent = percentage * 0.01
+    val textColor = if (percentage > 86) Color.White else Color.Black
+    val iconColor = if (percentage > 45) Color.White else CustomColor.gray2
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
         Box(
             modifier = Modifier
                 .width(52.dp)
                 .height(80.dp)
-                .background(color = CustomColor.gray1, shape = RoundedCornerShape(size =5.dp))
-                .clickable {onVote()},
+                .background(color = CustomColor.gray1, shape = RoundedCornerShape(size = 5.dp))
+                .clickable { onVote() },
             contentAlignment = Alignment.Center
-        ){
+        ) {
 
             Box(
                 modifier = Modifier.fillMaxSize()
                     .background(color = Color.Transparent),
                 contentAlignment = Alignment.BottomCenter
-            ){
+            ) {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
                         .height((80 * percent).dp)
-                        .background(color = Color.Black, shape = RoundedCornerShape(bottomStart = 5.dp, bottomEnd = 5.dp))
+                        .background(
+                            color = Color.Black,
+                            shape = RoundedCornerShape(
+                                bottomStart = 5.dp,
+                                bottomEnd = 5.dp,
+                                topStart = 5.dp,
+                                topEnd = 5.dp
+                            )
+                        )
                 )
             }
             Icon(
                 modifier = Modifier.size(16.dp),
                 painter = icon,
                 contentDescription = "Main Image",
-                tint = Color(0xFFBBBBBB)
+                tint = iconColor
             )
             Box(
                 modifier = Modifier.fillMaxSize()
                     .background(color = Color.Transparent),
                 contentAlignment = Alignment.TopCenter,
-            ){
-                Spacer(Modifier.height(5.dp))
-
+            ) {
+                Spacer(Modifier.height(7.dp))
                 Text(
                     modifier = Modifier.height(12.dp),
                     text = "${percentage}%",
                     fontSize = 10.sp,
-                    fontWeight = FontWeight.Medium
+                    fontWeight = FontWeight.Medium,
+                    color = textColor
                 )
             }
         }
@@ -85,6 +91,6 @@ fun VoteView(
 
 @Preview
 @Composable
-fun VoteViewPreview(){
-    VoteView(60, icon = painterResource(R.drawable.ic_winter),title= "겨울",{})
+fun VoteViewPreview() {
+    VoteView(89, icon = painterResource(R.drawable.ic_winter), title = "겨울", {})
 }

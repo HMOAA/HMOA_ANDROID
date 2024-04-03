3a8e9dbc5fd1151a65515294a32fd541ae8ca484
이용인 <yongin0517@gmail.com>
2024-04-03 17:00:43 +0900

    Ignore: import 및 줄바꿈

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/ImageView.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/ImageView.kt
@@ -1,25 +1,24 @@
 package com.hmoa.core_designsystem.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 @Composable
-fun ImageView(imageUrl: String?, width: Float, height: Float, backgroundColor: Color,contentScale: ContentScale) {
+fun ImageView(imageUrl: String?, width: Float, height: Float, backgroundColor: Color, contentScale: ContentScale) {
     Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         com.skydoves.landscapist.glide.GlideImage(
-            imageModel = imageUrl?: "",
+            imageModel = imageUrl ?: "",
             modifier = Modifier.fillMaxWidth(width).fillMaxHeight(height).background(color = backgroundColor),
             contentScale = contentScale
         )
     }
 }
+

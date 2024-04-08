644877b86b13db134bdb27df9334fe39798277ff
이용인 <yongin0517@gmail.com>
2024-04-08 15:18:32 +0900

    Fix: token 얻는 과정에서 viewmodelScope 추가

--- app/src/main/java/com/hmoa/app/AppViewModel.kt
+++ app/src/main/java/com/hmoa/app/AppViewModel.kt
@@ -2,19 +2,26 @@ package com.hmoa.app
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hmoa.app.navigation.MAIN_ROUTE
 import com.hmoa.core_domain.usecase.GetAuthAndRememberedTokenUseCase
 import com.hmoa.feature_authentication.navigation.LOGIN_ROUTE
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import javax.inject.Inject
 
 @HiltViewModel
 class AppViewModel @Inject constructor(private val getAuthAndRememberedTokenUseCase: GetAuthAndRememberedTokenUseCase) :
     ViewModel() {
+    private var authToken: String? = null
+    private var rememberedToken: String? = null
     suspend fun routeInitialScreen(): String {
-        val pair = getAuthAndRememberedTokenUseCase()
-        val authToken = pair.first
-        val rememberedToken = pair.second
+        viewModelScope.async {
+            val pair = getAuthAndRememberedTokenUseCase()
+            authToken = pair.first
+            rememberedToken = pair.second
+        }.await()
+
         Log.d("TAG TEST", "token : ${authToken} / remember : ${rememberedToken}")
 
         if (authToken == null && rememberedToken == null) {

71cde0318d9a8ba2657c6ca3dd908ef4f9f89a0a
uselessNaming <seohojon@naver.com>
2024-04-09 15:59:16 +0900

    Refactor: 코드 구조 수정

--- feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityPostPage.kt
+++ feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityPostPage.kt
@@ -1,5 +1,6 @@
 package com.hmoa.feature_community.Screen
 
+import android.content.Context
 import android.net.Uri
 import android.util.Log
 import android.widget.Toast
@@ -34,6 +35,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -122,7 +124,6 @@ fun CommunityPostRoute(
     )
 }
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalLayoutApi::class)
 @Composable
 fun PostCommunityPage(
     errState : String?,
@@ -130,36 +131,82 @@ fun PostCommunityPage(
     onTitleChanged : (String) -> Unit,
     content : String,
     onContentChanged : (String) -> Unit,
-    //Floating Button에서 받아와야 함
     category : Category,
     pictures : List<Uri>,
     onUpdatePictures : (List<Uri>) -> Unit,
     onDeletePictures : (Int) -> Unit,
-    //뒤로가기
     onNavBack: () -> Unit,
-    //해당 게시글 Post
     onPostCommunity: () -> Unit,
 ) {
-
-    //갤러리에서 사진 가져오기
-    val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickMultipleVisualMedia(),
-        onResult = {uris ->
-            Log.d("TAG TEST", "uris : ${uris}")
-            onUpdatePictures(uris)
-        }
-    )
-
-    //pager state
-    val state = rememberPagerState(
-        initialPage = 0,
-        pageCount = { pictures.size }
-    )
-
     val scrollableState = rememberScrollState()
 
     val context = LocalContext.current
 
+    val isDataEmpty by remember{
+        mutableStateOf(title.isNotEmpty() && content.isNotEmpty())
+    }
+
+    when(errState) {
+        null -> {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ){
+                //unique top bar
+                CommunityPostTopBar(
+                    context = context,
+                    title = category.name,
+                    isDataEmpty = isDataEmpty,
+                    onPostCommunity = onPostCommunity,
+                    onNavBack = onNavBack
+                )
+
+                HorizontalDivider(
+                    Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = Color.Black
+                )
+
+                TextFieldTitle(
+                    title = title,
+                    onTitleChanged = onTitleChanged
+                )
+                //title input
+
+                HorizontalDivider(
+                    Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = Color.Black
+                )
+
+                TextFieldContent(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .padding(horizontal = 33.dp, vertical = 27.dp)
+                        .scrollable(state = scrollableState, orientation = Orientation.Horizontal),
+                    content = content,
+                    onContentChanged = onContentChanged,
+                    pictures = pictures,
+                    onDeletePictures = onDeletePictures
+                )
+
+                BottomCameraBtn(onUpdatePictures)
+            }
+        }
+        else -> {
+            /** err state에 대한 메세지를 띄우고 onNavBack() 하는 것이 좋을 듯 */
+        }
+    }
+}
+
+@Composable
+fun CommunityPostTopBar(
+    context : Context,
+    title : String,
+    isDataEmpty : Boolean,
+    onPostCommunity : () -> Unit,
+    onNavBack : () -> Unit
+){
     val sideTopBarTextStyle = TextStyle(
         fontSize = 16.sp,
         color = Color.Black
@@ -168,6 +215,51 @@ fun PostCommunityPage(
         fontSize = 18.sp,
         color = Color.Black
     )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            modifier = Modifier.clickable{
+                onNavBack()
+            },
+            text = "취소",
+            style = sideTopBarTextStyle
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        Text(
+            text = title,
+            style = mainTopBarTextStyle
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        Text(
+            modifier = Modifier.clickable{
+                if (isDataEmpty){
+                    onPostCommunity()
+                    onNavBack()
+                } else {
+                    Toast.makeText(context, "제목이나 내용을 빈 칸 없이 채워주세요",Toast.LENGTH_SHORT).show()
+                }
+            },
+            text = "확인",
+            style = sideTopBarTextStyle
+        )
+    }
+}
+
+@Composable
+fun TextFieldTitle(
+    title : String,
+    onTitleChanged : (String) -> Unit,
+){
     val titleTextIntroTextStyle = TextStyle(
         fontSize = 16.sp,
         color = Color.Black
@@ -176,6 +268,71 @@ fun PostCommunityPage(
         fontSize = 14.sp,
         color = Color.Black
     )
+    val placeholderTextStyle = TextStyle(
+        fontSize = 14.sp,
+        color = CustomColor.gray3
+    )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(45.dp)
+            .padding(start = 33.dp, end = 15.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = "제목:",
+            style = titleTextIntroTextStyle
+        )
+
+        Spacer(Modifier.width(6.dp))
+
+        BasicTextField(
+            modifier = Modifier.weight(1f),
+            value = title,
+            onValueChange = {
+                //글자 수 제한 20
+                if (title.length < 20) {
+                    onTitleChanged(it)
+                }
+            },
+            textStyle = titleInputTextStyle,
+            maxLines = 1,
+            singleLine = true,
+            cursorBrush = SolidColor(CustomColor.gray3)
+        ){
+            //placeholder
+            if (title.isEmpty()){
+                Text(
+                    text = "제목을 입력해주세요",
+                    style = placeholderTextStyle
+                )
+            }
+            it()
+        }
+
+        Text(
+            text = "${title.length}/20",
+            style = titleInputTextStyle
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TextFieldContent(
+    modifier : Modifier,
+    content : String,
+    onContentChanged : (String) -> Unit,
+    pictures : List<Uri>,
+    onDeletePictures : (Int) -> Unit,
+){
+    //pager state
+    val state = rememberPagerState(
+        initialPage = 0,
+        pageCount = { pictures.size }
+    )
+
     val contentInputTextStyle = TextStyle(
         fontSize = 14.sp,
         color = Color.Black
@@ -185,238 +342,112 @@ fun PostCommunityPage(
         color = CustomColor.gray3
     )
 
-    when(errState) {
-        null -> {
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ){
-                //unique top bar
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(60.dp)
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ){
-                    Text(
-                        modifier = Modifier.clickable{
-                            onNavBack()
-                        },
-                        text = "취소",
-                        style = sideTopBarTextStyle
-                    )
-
-                    Spacer(Modifier.weight(1f))
-
-                    Text(
-                        text = category.name,
-                        style = mainTopBarTextStyle
-                    )
-
-                    Spacer(Modifier.weight(1f))
-
-                    Text(
-                        modifier = Modifier.clickable{
-                            if (title.isNotEmpty() && content.isNotEmpty()){
-                                onPostCommunity()
-                                onNavBack()
-                            } else {
-                                Toast.makeText(context, "제목이나 내용을 빈 칸 없이 채워주세요",Toast.LENGTH_SHORT).show()
-                            }
-                        },
-                        text = "확인",
-                        style = sideTopBarTextStyle
-                    )
-                }
-
-                HorizontalDivider(
-                    Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = Color.Black
+    Column(
+        modifier = modifier
+    ){
+        //content input
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            value = content,
+            onValueChange = {
+                onContentChanged(it)
+            },
+            textStyle = contentInputTextStyle,
+        ){
+            //placeholder
+            if (content.isEmpty()){
+                Text(
+                    text = "내용을 입력해주세요",
+                    style = placeholderTextStyle
                 )
+            }
+            it()
+        }
 
-                //title input
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(45.dp)
-                        .padding(start = 33.dp, end = 15.dp),
-                    verticalAlignment = Alignment.CenterVertically
+        if(pictures.isNotEmpty()){
+
+            Spacer(Modifier.height(10.dp))
+
+            HorizontalPager(
+                modifier = Modifier.size(274.dp),
+                state = state
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize()
                 ){
-                    Text(
-                        text = "제목:",
-                        style = titleTextIntroTextStyle
+                    //image view
+                    ImageView(
+                        imageUrl = pictures[it].toString(),
+                        width = 274f,
+                        height = 274f,
+                        backgroundColor = CustomColor.gray1,
+                        contentScale = ContentScale.Crop
                     )
 
-                    Spacer(Modifier.width(6.dp))
-
-                    BasicTextField(
-                        modifier = Modifier.weight(1f),
-                        value = title,
-                        onValueChange = {
-                            //글자 수 제한 20
-                            if (title.length < 20) {
-                                onTitleChanged(it)
-                            }
-                        },
-                        textStyle = titleInputTextStyle,
-                        maxLines = 1,
-                        singleLine = true,
-                        cursorBrush = SolidColor(CustomColor.gray3)
-                    ){
-                        //placeholder
-                        if (title.isEmpty()){
-                            Text(
-                                text = "제목을 입력해주세요",
-                                style = placeholderTextStyle
-                            )
-                        }
-                        it()
-                    }
-
-                    Text(
-                        text = "${title.length}/20",
-                        style = titleInputTextStyle
-                    )
-                }
-
-                HorizontalDivider(
-                    Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = Color.Black
-                )
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .padding(horizontal = 33.dp, vertical = 27.dp)
-                        .scrollable(state = scrollableState, orientation = Orientation.Horizontal)
-                ){
-                    //content input
-                    BasicTextField(
+                    //삭제 버튼
+                    Row(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f),
-                        value = content,
-                        onValueChange = {
-                            onContentChanged(it)
-                        },
-                        textStyle = contentInputTextStyle,
+                            .fillMaxSize()
+                            .padding(top = 15.dp, end = 15.dp),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.Top
                     ){
-                        //placeholder
-                        if (content.isEmpty()){
-                            Text(
-                                text = "내용을 입력해주세요",
-                                style = placeholderTextStyle
-                            )
-                        }
-                        it()
-                    }
-
-                    if(pictures.isNotEmpty()){
-
-                        Spacer(Modifier.height(10.dp))
-
-                        HorizontalPager(
-                            modifier = Modifier.size(274.dp),
-                            state = state
-                        ) {
-                            Box(
-                                modifier = Modifier.fillMaxSize()
-                            ){
-                                //image view
-                                ImageView(
-                                    imageUrl = pictures[it].toString(),
-                                    width = 274f,
-                                    height = 274f,
-                                    backgroundColor = CustomColor.gray1,
-                                    contentScale = ContentScale.Crop
-                                )
-
-                                //삭제 버튼
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(top = 15.dp, end = 15.dp),
-                                    horizontalArrangement = Arrangement.End,
-                                    verticalAlignment = Alignment.Top
-                                ){
-                                    IconButton(
-                                        modifier = Modifier.size(24.dp),
-                                        onClick = {
-                                            onDeletePictures(it)
-                                        }
-                                    ) {
-                                        Icon(
-                                            modifier = Modifier.fillMaxSize(),
-                                            imageVector = Icons.Filled.Close,
-                                            contentDescription = "Delete Button",
-                                            tint = CustomColor.red
-                                        )
-                                    }
-                                }
+                        IconButton(
+                            modifier = Modifier.size(24.dp),
+                            onClick = {
+                                onDeletePictures(it)
                             }
+                        ) {
+                            Icon(
+                                modifier = Modifier.fillMaxSize(),
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "Delete Button",
+                                tint = CustomColor.red
+                            )
                         }
                     }
-                }
-
-                /** keyboard 옵션으로 보이도록 View 변경해야 함 */
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
-                    onClick = {
-                        multiplePhotoPickerLauncher.launch(
-                            PickVisualMediaRequest(
-                                ActivityResultContracts.PickVisualMedia.ImageOnly
-                            )
-                        )
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = CustomColor.gray1
-                    ),
-                    shape = RectangleShape
-                ){
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "사진을 추가하려면 눌러주세요!",
-                        textAlign = TextAlign.Center,
-                        color = Color.Blue,
-                        fontSize = 20.sp
-                    )
                 }
             }
-        }
-        else -> {
-            /** err state에 대한 메세지를 띄우고 onNavBack() 하는 것이 좋을 듯 */
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun TestPostCommunityPage() {
-
-    var title by remember{mutableStateOf("")}
-    var content by remember{mutableStateOf("")}
-    var test by remember{ mutableStateOf("") }
-
-    PostCommunityPage(
-        errState = null,
-        title = title,
-        onTitleChanged = {
-            title = it
-        },
-        content = content,
-        onContentChanged = {
-            content = it
-        },
-        category = Category.추천,
-        onNavBack = {},
-        pictures = arrayListOf(),
-        onUpdatePictures = {},
-        onDeletePictures = {},
-        onPostCommunity = {}
+fun BottomCameraBtn(
+    onUpdatePictures : (List<Uri>) -> Unit,
+){
+    //갤러리에서 사진 가져오기
+    val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(),
+        onResult = {uris ->
+            onUpdatePictures(uris)
+        }
     )
+    /** keyboard 옵션으로 보이도록 View 변경해야 함 */
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        onClick = {
+            multiplePhotoPickerLauncher.launch(
+                PickVisualMediaRequest(
+                    ActivityResultContracts.PickVisualMedia.ImageOnly
+                )
+            )
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = CustomColor.gray1
+        ),
+        shape = RectangleShape
+    ){
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "사진을 추가하려면 눌러주세요!",
+            textAlign = TextAlign.Center,
+            color = Color.Blue,
+            fontSize = 20.sp
+        )
+    }
 }

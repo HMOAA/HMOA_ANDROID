3298bdcfb6485b27bc091b997b5288dc9889e520
uselessNaming <seohojon@naver.com>
2024-04-09 15:25:06 +0900

    Feat: Community Paging Source 추가

--- feature-community/src/main/java/com/hmoa/feature_community/ViewModel/CommunityMainViewModel.kt
+++ feature-community/src/main/java/com/hmoa/feature_community/ViewModel/CommunityMainViewModel.kt
@@ -2,48 +2,52 @@ package com.hmoa.feature_community.ViewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.cachedIn
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.hmoa.core_common.Result
 import com.hmoa.core_common.asResult
 import com.hmoa.core_domain.repository.CommunityRepository
 import com.hmoa.core_model.Category
 import com.hmoa.core_model.response.CommunityByCategoryResponseDto
+import com.hmoa.feature_community.CommunityPagingSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
+const val PAGE_SIZE = 10
+
 @HiltViewModel
 class CommunityMainViewModel @Inject constructor(
-    private val repository: CommunityRepository
+    private val communityRepository: CommunityRepository
 ) : ViewModel() {
 
     //type 정보
     private val _type = MutableStateFlow(Category.추천)
     val type get() = _type.asStateFlow()
 
-    //page
-    private val _page = MutableStateFlow(0)
-    val page get() = _page.asStateFlow()
-
     private val _errState = MutableStateFlow("")
     val errState get() = _errState.asStateFlow()
 
-    val uiState : StateFlow<CommunityMainUiState> = type.combine(page){ type, page ->
-        val result = repository.getCommunityByCategory(type.name, page)
-        if (result.exception is Exception) {
-            throw result.exception!!
-        }
-        result.data
+    val uiState : StateFlow<CommunityMainUiState> = combine(type){
+        Pager(
+            config = PagingConfig(PAGE_SIZE),
+            pagingSourceFactory = {getCommunityPaging(type.value.name)}
+        ).flow.cachedIn(viewModelScope)
     }.asResult().map{
         when (it) {
             is Result.Loading -> CommunityMainUiState.Loading
             is Result.Success -> {
-                CommunityMainUiState.Community(it.data!!)
+                CommunityMainUiState.Community(it.data)
             }
             is Result.Error -> CommunityMainUiState.Error
         }
     }.stateIn(
         scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(1_000),
+        started = SharingStarted.WhileSubscribed(3_000),
         initialValue = CommunityMainUiState.Loading
     )
 
@@ -52,17 +56,16 @@ class CommunityMainViewModel @Inject constructor(
         _type.update { category }
     }
 
-    //page 정보 변경
-    fun addPage() {
-        _page.update { _page.value + 1 }
-    }
-
+    private fun getCommunityPaging(category : String) = CommunityPagingSource(
+        communityRepository = communityRepository,
+        category = category
+    )
 }
 
 sealed interface CommunityMainUiState {
     data object Loading : CommunityMainUiState
     data class Community(
-        val communities: List<CommunityByCategoryResponseDto>
+        val communities: Flow<PagingData<CommunityByCategoryResponseDto>>
     ) : CommunityMainUiState
 
     data object Error : CommunityMainUiState

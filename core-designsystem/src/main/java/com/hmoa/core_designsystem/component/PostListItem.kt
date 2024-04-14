e0ff3631877c1f3dd196455da7a0bd40b2810464
uselessNaming <seohojon@naver.com>
2024-04-14 15:53:00 +0900

    Design: Comment Count, Heart Count UI 추가

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/PostListItem.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/PostListItem.kt
@@ -1,8 +1,10 @@
 package com.hmoa.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,7 +25,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +43,8 @@ fun PostListItem(
     onPostClick : () -> Unit,
     postType : String,
     postTitle : String,
+    heartCount : Int,
+    commentCount : Int,
 ){
     Row(
         modifier = modifier
@@ -54,11 +60,36 @@ fun PostListItem(
                 .weight(1f),
             verticalArrangement = Arrangement.Center
         ){
-            Text(
-                text = postType,
-                fontSize = 14.sp,
-                color = CustomColor.gray2
-            )
+            Row(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = postType,
+                    fontSize = 14.sp,
+                    color = CustomColor.gray2
+                )
+
+                Spacer(Modifier.weight(1f))
+
+                if (heartCount > 0) {
+                    CountBadge(
+                        icon = R.drawable.ic_heart_filled,
+                        count = heartCount
+                    )
+                }
+
+                Spacer(Modifier.width(8.dp))
+
+                if (commentCount > 0) {
+                    CountBadge(
+                        icon = R.drawable.ic_kakao,
+                        count = commentCount
+                    )
+                }
+            }
 
             Spacer(Modifier.height(8.dp))
 
@@ -75,13 +106,62 @@ fun PostListItem(
 @Preview(showBackground = true)
 @Composable
 fun TestPostListItem(){
-    PostListItem (
-        Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .border(width = 1.dp, color = CustomColor.gray2, shape = RoundedCornerShape(10.dp)),
-        onPostClick = {},
-        postType = "추천해주세요",
-        postTitle = "여자친구한테 선물할 향수 뭐가 좋을까요?",
-    )
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        PostListItem (
+            Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .border(width = 1.dp, color = CustomColor.gray2, shape = RoundedCornerShape(10.dp)),
+            onPostClick = {},
+            postType = "추천해주세요",
+            postTitle = "여자친구한테 선물할 향수 뭐가 좋을까요?",
+            heartCount = 10,
+            commentCount = 10
+        )
+        Spacer(Modifier.height(10.dp))
+        PostListItem (
+            Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .border(width = 1.dp, color = CustomColor.gray2, shape = RoundedCornerShape(10.dp)),
+            onPostClick = {},
+            postType = "추천해주세요",
+            postTitle = "여자친구한테 선물할 향수 뭐가 좋을까요?",
+            heartCount = 10,
+            commentCount = 0
+        )
+
+    }
+}
+
+@Composable
+fun CountBadge(
+    icon : Int,
+    count : Int
+){
+    Row(
+        modifier = Modifier
+            .width(40.dp)
+            .height(20.dp)
+            .background(color = CustomColor.gray1, shape = RoundedCornerShape(size = 20.dp))
+            .clip(RoundedCornerShape(size = 20.dp)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ){
+        Icon(
+            modifier = Modifier.size(12.dp),
+            painter = painterResource(icon),
+            contentDescription = "Count Badge"
+        )
+
+        Spacer(Modifier.width(4.dp))
+
+        Text(
+            text = count.toString(),
+            fontSize = 12.sp,
+            color = Color.Black,
+        )
+    }
 }

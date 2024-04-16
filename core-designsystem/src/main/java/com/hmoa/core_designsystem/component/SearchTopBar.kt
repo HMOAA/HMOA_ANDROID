c78428ac89689577a1db6b5aa87d23d44401a9cc
이용인 <yongin0517@gmail.com>
2024-04-16 23:09:52 +0900

    Feat: SearchTopBar onClickSearch 콜백 매개변수 추가

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/SearchTopBar.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/SearchTopBar.kt
@@ -2,26 +2,13 @@ package com.hmoa.core_designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,12 +24,12 @@ import com.hmoa.core_designsystem.theme.CustomColor
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchTopBar(
-    searchWord : String,
-    onChangeWord : (String) -> Unit,
-    onClearWord : () -> Unit,
-    onClickSearch : () -> Unit,
-    onNavBack : () -> Unit,
-){
+    searchWord: String,
+    onChangeWord: (String) -> Unit,
+    onClearWord: () -> Unit,
+    onClickSearch: (String) -> Unit,
+    onNavBack: () -> Unit,
+) {
 
     TopAppBar(
         title = {
@@ -58,11 +45,11 @@ fun SearchTopBar(
                 textStyle = TextStyle(
                     fontSize = 16.sp,
                 ),
-            ){
+            ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
-                ){
-                    if(searchWord.isEmpty()){
+                ) {
+                    if (searchWord.isEmpty()) {
                         Text(
                             text = "키워드를 검색해보세요",
                             fontSize = 16.sp,
@@ -83,30 +70,29 @@ fun SearchTopBar(
             ) {
                 Icon(
                     modifier = Modifier.fillMaxSize(),
-                    painter = painterResource(R.drawable.ic_back), 
+                    painter = painterResource(R.drawable.ic_back),
                     contentDescription = "Back Button"
                 )
             }
         },
         actions = {
 
-            if (searchWord.isNotEmpty()){
+            if (searchWord.isNotEmpty()) {
                 IconButton(
                     modifier = Modifier.size(20.dp)
                         .background(color = CustomColor.gray2, shape = CircleShape)
                         .clip(CircleShape),
                     onClick = onClearWord
-                ){
+                ) {
                     Icon(
                         imageVector = Icons.Filled.Close,
                         contentDescription = "Close Button",
                         tint = Color.White
                     )
                 }
-            }
-            else {
+            } else {
                 Icon(
-                    modifier = Modifier.size(20.dp).clickable { onClickSearch() },
+                    modifier = Modifier.size(20.dp).clickable { onClickSearch(searchWord) },
                     painter = painterResource(R.drawable.ic_search),
                     contentDescription = "Search Button",
                     tint = CustomColor.gray3
@@ -114,30 +100,36 @@ fun SearchTopBar(
             }
             Spacer(Modifier.width(16.dp))
         },
-        colors = TopAppBarColors(containerColor = Color.White, actionIconContentColor = CustomColor.gray3, titleContentColor = Color.Black, scrolledContainerColor = Color.White, navigationIconContentColor = CustomColor.gray3)
+        colors = TopAppBarColors(
+            containerColor = Color.White,
+            actionIconContentColor = CustomColor.gray3,
+            titleContentColor = Color.Black,
+            scrolledContainerColor = Color.White,
+            navigationIconContentColor = Color.Black
+        )
     )
 
 }
 
 @Preview
 @Composable
-fun TestSearchBar(){
+fun TestSearchBar() {
 
-    var content by remember{mutableStateOf("test")}
+    var content by remember { mutableStateOf("test") }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.DarkGray),
         contentAlignment = Alignment.Center
-    ){
+    ) {
         SearchTopBar(
             searchWord = content,
             onChangeWord = {
                 content = it
             },
             onClickSearch = {},
-            onClearWord = {content = ""},
+            onClearWord = { content = "" },
             onNavBack = {}
         )
     }

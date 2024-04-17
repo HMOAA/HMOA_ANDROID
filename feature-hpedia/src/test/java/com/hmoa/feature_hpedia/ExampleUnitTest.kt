c28d3fa1f2853aa586003a16c17e8a1983a45d7d
uselessNaming <seohojon@naver.com>
2024-04-17 22:01:35 +0900

    Test: Enum Class 명 테스트

--- feature-hpedia/src/test/java/com/hmoa/feature_hpedia/ExampleUnitTest.kt
+++ feature-hpedia/src/test/java/com/hmoa/feature_hpedia/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.hmoa.feature_hpedia
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,21 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    enum class A{
+        a,
+        b,
+        c,
+        d
+    }
+
+    enum class B{
+        a,
+        b,
+        c,
+        d
+    }
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(A.a.name == B.a.name, true)
     }
 }

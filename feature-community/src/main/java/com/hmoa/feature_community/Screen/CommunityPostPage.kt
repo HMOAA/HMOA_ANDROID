220346762154ed3e0e9891011f98127b0dffda41
uselessNaming <seohojon@naver.com>
2024-04-19 23:00:24 +0900

    Remove: 불필요 파라미터 삭제

--- feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityPostPage.kt
+++ feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityPostPage.kt
@@ -2,10 +2,8 @@ package com.hmoa.feature_community.Screen
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
@@ -15,8 +13,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -34,18 +30,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -114,10 +105,6 @@ fun PostCommunityPage(
 
     val context = LocalContext.current
 
-    val isDataEmpty by remember{
-        mutableStateOf(title.isNotEmpty() && content.isNotEmpty())
-    }
-
     when(errState) {
         null -> {
             Column(
@@ -127,27 +114,19 @@ fun PostCommunityPage(
                 CommunityPostTopBar(
                     context = context,
                     title = category.name,
-                    isDataEmpty = isDataEmpty,
+                    isDataEmpty = title.isNotEmpty() && content.isNotEmpty(),
                     onPostCommunity = onPostCommunity,
                     onNavBack = onNavBack
                 )
 
-                HorizontalDivider(
-                    Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = Color.Black
-                )
+                HorizontalDivider(Modifier.fillMaxWidth(),thickness = 1.dp,color = Color.Black)
 
                 TextFieldTitle(
                     title = title,
                     onTitleChanged = onTitleChanged
                 )
 
-                HorizontalDivider(
-                    Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = Color.Black
-                )
+                HorizontalDivider(Modifier.fillMaxWidth(),thickness = 1.dp,color = Color.Black)
 
                 TextFieldContent(
                     modifier = Modifier

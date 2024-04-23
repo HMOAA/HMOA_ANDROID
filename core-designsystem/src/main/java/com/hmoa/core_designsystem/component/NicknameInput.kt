7ce18aca314235ea54f573609812d8091761514e
uselessNaming <seohojon@naver.com>
2024-04-23 14:03:01 +0900

    Fix: 초기 값 지정 가능하도록 변경

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/NicknameInput.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/NicknameInput.kt
@@ -18,8 +18,15 @@ import androidx.compose.ui.unit.sp
 import com.hmoa.core_designsystem.theme.CustomColor
 
 @Composable
-fun NicknameInput(onPressNicknameExist: (text: String) -> Unit, isAvailable: Boolean) {
-    var nickname by remember { mutableStateOf("") }
+fun NicknameInput(
+    initNickname : String? = null,
+    onPressNicknameExist: (text: String) -> Unit,
+    isAvailable: Boolean,
+    isEnabled : Boolean = true,
+) {
+    var nickname by remember {
+        mutableStateOf(initNickname ?: "")
+    }
     var descriptionText by remember { mutableStateOf("닉네임 제한 캡션입니다") }
     var descriptionTextColor by remember { mutableStateOf(Color.Black) }
     var nicknameLength by remember { mutableStateOf(nickname.length.toString()) }
@@ -84,14 +91,14 @@ fun NicknameInput(onPressNicknameExist: (text: String) -> Unit, isAvailable: Boo
                     }
                     Divider(modifier = Modifier.border(width = 1.dp, color = CustomColor.gray2).fillMaxWidth(0.75f))
                 }
-                Column(modifier = Modifier.padding(start = 8.dp)) {
+                Column(modifier = Modifier.padding(start = 8.dp), verticalArrangement = Arrangement.Center) {
                     Button(
-                        true,
+                        isEnabled,
                         "중복확인",
                         { onPressNicknameExist(nickname) },
                         textSize = 14,
                         radious = 10,
-                        buttonModifier = Modifier.height(46.dp).fillMaxWidth(1f)
+                        buttonModifier = Modifier.height(46.dp).fillMaxWidth(1f),
                     )
                 }
             }
@@ -129,5 +136,5 @@ fun handleTextColor(isAvailable: Boolean): Color {
 @Preview
 @Composable
 fun NicknameInputPreview() {
-    NicknameInput({}, true)
+    NicknameInput(null, {}, true)
 }

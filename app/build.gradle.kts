048541cca6f756935a6d615fb09026d7e03c0b78
이용인 <yongin0517@gmail.com>
2024-04-25 04:54:49 +0900

    Chore: firebase-messaging 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,9 @@ dependencies {
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-messaging:21.1.0")
     implementation("com.google.firebase:firebase-analytics")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

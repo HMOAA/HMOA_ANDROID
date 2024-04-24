416842aaf57b26efaa051ecb144dda6d0bd992cd
이용인 <yongin0517@gmail.com>
2024-04-25 01:01:29 +0900

    Chore: paging-compose 버전 변경

--- feature-home/build.gradle.kts
+++ feature-home/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 
     implementation("androidx.compose.material:material:1.2.0-beta02")
     implementation("androidx.compose.material3:material3:1.1.0")
-    implementation("androidx.paging:paging-compose:1.0.0-alpha02")
+    implementation("androidx.paging:paging-compose:3.2.0")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui:1.1.0")
     implementation("androidx.navigation:navigation-compose:2.7.0")

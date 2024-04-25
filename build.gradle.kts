68e4b5c2cc7f9333307ea993a23eb4c4aad12d29
이용인 <yongin0517@gmail.com>
2024-04-25 13:18:15 +0900

    Chore: 구글플레이 서비스 gradle 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 plugins {

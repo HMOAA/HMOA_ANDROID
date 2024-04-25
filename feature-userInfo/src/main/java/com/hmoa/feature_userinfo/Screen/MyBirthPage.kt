120b0896f676ffca6051bdc08568cfa98d8c1f27
uselessNaming <seohojon@naver.com>
2024-04-25 18:04:25 +0900

    Design: 버튼 UI 변경, 불필요 코드 제거

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/Screen/MyBirthPage.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/Screen/MyBirthPage.kt
@@ -1,10 +1,17 @@
 package com.example.feature_userinfo
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -19,8 +26,8 @@ import com.hmoa.core_designsystem.component.Button
 import com.hmoa.core_designsystem.theme.CustomColor
 import com.hmoa.feature_userinfo.viewModel.MyBirthUiState
 import com.hmoa.feature_userinfo.viewModel.MyBirthViewModel
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
-
 @Composable
 fun MyBirthRoute(
     onNavBack: () -> Unit,
@@ -81,6 +88,7 @@ fun MyBirthPage(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun SelectBirthContent(
     availableYearRange : List<Int>,
@@ -92,58 +100,77 @@ private fun SelectBirthContent(
     onSaveBirth: () -> Unit,
     onNavBack: () -> Unit
 ){
-    if (showDialog) {
-        YearPickerDialog(
-            yearList = availableYearRange,
-            initialValue = birth,
-            height = 370.dp,
-            onDismiss = onChangeDialogState,
-            onDoneClick = {
-                onUpdateBirth(it)
-                onChangeDialogState()
+    val scope = rememberCoroutineScope()
+    val modalSheetState = androidx.compose.material.rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = {it!=ModalBottomSheetValue.HalfExpanded},
+        skipHalfExpanded = true
+    )
+    Log.d("TAG TEST", "showDialog : ${showDialog}")
+    ModalBottomSheetLayout(
+        sheetState = modalSheetState,
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(CustomColor.gray4),
+                verticalArrangement = Arrangement.Bottom
+            ){
+                YearPickerDialog(
+                    yearList = availableYearRange,
+                    initialValue = birth,
+                    height = 370.dp,
+                    onDismiss = {scope.launch{modalSheetState.hide()}},
+                    onDoneClick = {
+                        onUpdateBirth(it)
+                        scope.launch{modalSheetState.hide()}
+                    }
+                )
             }
-        )
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White)
+        }
     ) {
-        TopBar(
-            navIcon = painterResource(com.hmoa.core_designsystem.R.drawable.ic_back),
-            onNavClick = onNavBack,
-            title = "출생연도"
-        )
-        Spacer(Modifier.height(36.dp))
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(horizontal = 16.dp)
+                .fillMaxSize()
+                .background(color = Color.White)
         ) {
-            Text(
-                text = "출생연도",
-                fontSize = 16.sp,
-                color = CustomColor.gray4
+            TopBar(
+                navIcon = painterResource(com.hmoa.core_designsystem.R.drawable.ic_back),
+                onNavClick = onNavBack,
+                title = "출생연도"
             )
-            Spacer(Modifier.height(8.dp))
-            Spinner(
-                width = 152.dp,
-                height = 46.dp,
-                value = birth,
+            Spacer(Modifier.height(36.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "출생연도",
+                    fontSize = 16.sp,
+                    color = CustomColor.gray4
+                )
+                Spacer(Modifier.height(8.dp))
+                Spinner(
+                    width = 152.dp,
+                    height = 46.dp,
+                    value = birth,
+                    onClick = {scope.launch{modalSheetState.show()}},
+                    placeholder = "선택"
+                )
+            }
+            Button(
+                buttonModifier = Modifier
+                    .fillMaxWidth()
+                    .height(82.dp),
+                isEnabled = isEnabled,
+                btnText = "변경",
                 onClick = {
-                    onChangeDialogState()
-                },
-                placeholder = "선택"
+                    onSaveBirth()
+                    onNavBack()
+                }
             )
         }
-        Button(
-            isEnabled = isEnabled,
-            btnText = "변경",
-            onClick = {
-                onSaveBirth()
-                onNavBack()
-            }
-        )
     }
 }

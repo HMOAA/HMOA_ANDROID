554b62e74ea2a9bcbe708dd8ec8ea5ae82b368e0
uselessNaming <seohojon@naver.com>
2024-04-26 20:25:18 +0900

    Feat: Click 이벤트 추가

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeGridItem.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeGridItem.kt
@@ -2,40 +2,43 @@ package com.hmoa.core_designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LikeGridItem(
-    itemPicture : ImageVector,
+    itemPicture : String,
+    onClickItem : () -> Unit,
 ){
     Box(
         modifier = Modifier
             .size(104.dp)
             .border(width = 1.dp, color = Color(0xFFBBBBBB))
-            .background(color = Color.White),
+            .background(color = Color.White)
+            .clickable{onClickItem()},
         contentAlignment = Alignment.BottomEnd
     ){
-        Icon(
-            modifier = Modifier.fillMaxSize(),
-            imageVector = itemPicture,
-            contentDescription = "Item Picture"
+        ImageView(
+            imageUrl = itemPicture,
+            width = 1f,
+            height = 1f,
+            backgroundColor = Color.White,
+            contentScale = ContentScale.Fit
         )
         Box(
-            modifier = Modifier.size(22.dp)
+            modifier = Modifier
+                .size(22.dp)
                 .padding(end = 8.dp, bottom = 8.dp)
         ){
             Icon(
@@ -44,18 +47,5 @@ fun LikeGridItem(
                 contentDescription = "Favorite Icon"
             )
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TestLikeGridItem(){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        LikeGridItem(
-            itemPicture = Icons.Filled.Delete
-        )
     }
 }

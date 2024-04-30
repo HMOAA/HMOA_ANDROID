9e41b0ee9de0ffd21acbc4f11e6a8aaef4bb00df
uselessNaming <seohojon@naver.com>
2024-04-30 22:31:27 +0900

    Fix: FCM 알림 설정 로직 변경

--- app/src/main/java/com/hmoa/app/FcmAppService.kt
+++ app/src/main/java/com/hmoa/app/FcmAppService.kt
@@ -1,13 +1,13 @@
 package com.hmoa.app
 
-import android.Manifest
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
+import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -24,30 +24,59 @@ class FcmAppService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        Log.i("TAG TEST", "onMessageReceived : ${remoteMessage}")
 
-        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        if(remoteMessage.notification != null){
+            sendNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+        } else if (remoteMessage.data.isNotEmpty()) {
+            sendTopNotification(remoteMessage.data["title"].toString(), remoteMessage.data["body"].toString())
+        }
+    }
+    private fun sendNotification(title : String?, body : String?){
+        val uniId = (System.currentTimeMillis() / 7).toInt()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
 
-        val channel = NotificationChannel("HMOA", "HMOA", NotificationManager.IMPORTANCE_DEFAULT)
-        notificationManager.createNotificationChannel(channel)
+        val channelId = ""
 
-        val builder : NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, "HMOA")
-
-        val title = remoteMessage.notification?.title
-        val body = remoteMessage.notification?.body
-
-        builder.setContentTitle(title)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
             .setContentText(body)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setDefaults(Notification.DEFAULT_SOUND)
 
-        val notification = builder.build()
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
+        channel.apply{
+            setShowBadge(false)
         }
+        notificationManager.notify(uniId, notificationBuilder.build())
+    }
+    private fun sendTopNotification(title : String?, body : String?){
+        val CHANNEL_DEFAULT_IMPORTANCE = "channelId"
+        val ONGOING_NOTIFICATION = 1
 
-        notificationManager.notify(1, notification)
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE)
+
+        val notification = Notification.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(com.hmoa.core_designsystem.R.drawable.ic_app_default_1)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = NotificationChannel(CHANNEL_DEFAULT_IMPORTANCE, "HMOA Channel", NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
+
+        notificationManager.notify(ONGOING_NOTIFICATION, notification)
     }
 }

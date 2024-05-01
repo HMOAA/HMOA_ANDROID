14ec804b93f4be8fb1ed00c350ff2535c6bcbb76
이용인 <yongin0517@gmail.com>
2024-05-02 03:28:32 +0900

    Design: 커뮤니티 상단 바 수정

--- feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityHome.kt
+++ feature-community/src/main/java/com/hmoa/feature_community/Screen/CommunityHome.kt
@@ -3,15 +3,7 @@ package com.hmoa.feature_community.Screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,12 +26,12 @@ import com.hmoa.feature_community.ViewModel.CommunityHomeViewModel
 fun CommunityHomeRoute(
     onNavCommunityGraph: () -> Unit,
     onNavCommunityDescription: (Int) -> Unit,
-    viewModel : CommunityHomeViewModel = hiltViewModel(),
-){
-    
+    viewModel: CommunityHomeViewModel = hiltViewModel(),
+) {
+
     //ui state를 전달 >> 여기에 community list를 가지고 이를 통해 LazyColumn 이용
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
-    
+
     CommunityHome(
         uiState = uiState.value,
         onNavCommunityGraph = onNavCommunityGraph,
@@ -49,79 +41,80 @@ fun CommunityHomeRoute(
 
 @Composable
 fun CommunityHome(
-    uiState : CommunityHomeUiState, //이거 uiState로 이전해서 uiState에서 데이터 가져오는 방식으로
-    onNavCommunityGraph : () -> Unit, //카테고리 별 Community 화면으로 이동
-    onNavCommunityDescription : (Int) -> Unit, //해당 Community Id를 가진 Description 화면으로 이동
-){
+    uiState: CommunityHomeUiState, //이거 uiState로 이전해서 uiState에서 데이터 가져오는 방식으로
+    onNavCommunityGraph: () -> Unit, //카테고리 별 Community 화면으로 이동
+    onNavCommunityDescription: (Int) -> Unit, //해당 Community Id를 가진 Description 화면으로 이동
+) {
+    Column(modifier = Modifier.padding(horizontal = 16.dp).fillMaxSize()) {
+        CommunityTitleBar(onNavCommunityByCategory = onNavCommunityGraph)
+        when (uiState) {
+            is CommunityHomeUiState.Loading -> {
+                
+            }
 
-    when (uiState) {
-        is CommunityHomeUiState.Loading -> {
+            is CommunityHomeUiState.Community -> {
+                CommunityHomeContent(
+                    communities = uiState.communities,
+                    onNavCommunityDescription = onNavCommunityDescription
+                )
+            }
 
-        }
-        is CommunityHomeUiState.Community -> {
-            CommunityHomeContent(
-                communities = uiState.communities,
-                onNavCommunityByCategory = onNavCommunityGraph,
-                onNavCommunityDescription = onNavCommunityDescription
-            )
-        }
-        is CommunityHomeUiState.Error -> {
+            is CommunityHomeUiState.Error -> {
 
+            }
         }
     }
+}
+
+@Composable
+fun CommunityTitleBar(
+    onNavCommunityByCategory: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ) {
+        Text(
+            text = "Community",
+            fontSize = 16.sp,
+            color = Color.Black
+        )
+
+        Text(
+            modifier = Modifier.clickable {
+                onNavCommunityByCategory()
+            },
+            text = "전체보기",
+            fontSize = 12.sp,
+            color = Color.Black
+        )
+    }
+    Spacer(Modifier.height(16.dp))
 }
 
 @Composable
 fun CommunityHomeContent(
-    communities : List<CommunityByCategoryResponseDto>,
-    onNavCommunityByCategory : () -> Unit,
-    onNavCommunityDescription : (Int) -> Unit,
-){
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(horizontal = 16.dp)
-    ){
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.Bottom
-        ){
-            Text(
-                text = "Community",
-                fontSize = 16.sp,
-                color = Color.Black
-            )
-
-            Text(
-                modifier = Modifier.clickable{
-                    onNavCommunityByCategory()
-                },
-                text = "전체보기",
-                fontSize = 12.sp,
-                color = Color.Black
-            )
-        }
-
-        Spacer(Modifier.height(16.dp))
-
-        PostList(
-            communities = communities,
-            onNavCommunityDescription = onNavCommunityDescription
-        )
-    }
+    communities: List<CommunityByCategoryResponseDto>,
+    onNavCommunityDescription: (Int) -> Unit,
+) {
+    PostList(
+        communities = communities,
+        onNavCommunityDescription = onNavCommunityDescription
+    )
 }
 
 @Composable
 fun PostList(
-    communities : List<CommunityByCategoryResponseDto>,
+    communities: List<CommunityByCategoryResponseDto>,
     onNavCommunityDescription: (Int) -> Unit
-){
+) {
     LazyColumn(
         modifier = Modifier.background(color = Color.White)
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
-        items(communities){community ->
+    ) {
+        items(communities) { community ->
             PostListItem(
                 modifier = Modifier
                     .fillMaxWidth()

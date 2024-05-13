fee279697f3702393e3f24d4c12afd7a737126e6
uselessNaming <seohojon@naver.com>
2024-05-13 22:49:17 +0900

    Chore: Compose 설정

--- feature-magazine/build.gradle.kts
+++ feature-magazine/build.gradle.kts
@@ -32,6 +32,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+    buildFeatures{
+        compose = true
+    }
 }
 
 dependencies {

83ddd89952f06cc6c1f3d4f77f0e322ea81688d2
uselessNaming <seohojon@naver.com>
2024-05-13 23:12:30 +0900

    Feat: Magazine Service 추가

---
+++ core-network/src/main/java/com/hmoa/core_network/service/MagazineService.kt
@@ -0,0 +1,38 @@
+package com.hmoa.core_network.service
+
+import ResultResponse
+import com.hmoa.core_model.response.MagazineListResponseDto
+import com.hmoa.core_model.response.MagazineResponseDto
+import com.hmoa.core_model.response.MagazineTastingCommentResponseDto
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MagazineService {
+
+    @GET("/magazine/{magazineId}")
+    suspend fun getMagazine(
+        @Path("magazineId") magazineId : Int
+    ) : ApiResponse<ResultResponse<MagazineResponseDto>>
+
+    @PUT("/magazine/{magazineId}/like")
+    suspend fun putMagazineHeart(
+        @Path("magazineId") magazineId : Int
+    ) : ApiResponse<Any>
+
+    @DELETE("/magazine/{magazineId}/like")
+    suspend fun deleteMagazineHeart(
+        @Path("magazineId") magazineId : Int
+    ) : ApiResponse<Any>
+
+    @GET("/magazine/list")
+    suspend fun getMagazineList(
+        @Query("page") page : Int
+    ) : ApiResponse<MagazineListResponseDto>
+
+    @GET("/magazine/tastingComment")
+    suspend fun getMagazineTastingComment() : ApiResponse<MagazineTastingCommentResponseDto>
+}

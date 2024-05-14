e46be36f7bc7efdf4d9d77f9d5191c2445208c8e
uselessNaming <seohojon@naver.com>
2024-05-14 16:10:01 +0900

    Design: TopBar 투명도 파라미터 추가

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/TopBar.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/TopBar.kt
@@ -1,26 +1,19 @@
 package com.hmoa.component
 
-import android.graphics.Color
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -80,7 +73,7 @@ fun TopBar(
 }
 
 @Composable
-@Preview
+@Preview(showBackground = true)
 fun TopBarPreview(){
     TopBar(
         title = "댓글",

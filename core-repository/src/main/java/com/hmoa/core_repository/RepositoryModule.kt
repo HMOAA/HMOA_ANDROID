e71d3429b82d1d4c64945e94d9e2c177aa12195d
uselessNaming <seohojon@naver.com>
2024-05-14 11:04:03 +0900

    Feat: Repository DI

--- core-repository/src/main/java/com/hmoa/core_repository/RepositoryModule.kt
+++ core-repository/src/main/java/com/hmoa/core_repository/RepositoryModule.kt
@@ -1,6 +1,23 @@
 package com.hmoa.core_repository
 
-import com.hmoa.core_domain.repository.*
+import com.hmoa.core_domain.repository.AdminRepository
+import com.hmoa.core_domain.repository.BrandHPediaRepository
+import com.hmoa.core_domain.repository.BrandRepository
+import com.hmoa.core_domain.repository.CommunityCommentRepository
+import com.hmoa.core_domain.repository.CommunityRepository
+import com.hmoa.core_domain.repository.FcmRepository
+import com.hmoa.core_domain.repository.LoginRepository
+import com.hmoa.core_domain.repository.MagazineRepository
+import com.hmoa.core_domain.repository.MainRepository
+import com.hmoa.core_domain.repository.MemberRepository
+import com.hmoa.core_domain.repository.NoteRepository
+import com.hmoa.core_domain.repository.PerfumeCommentRepository
+import com.hmoa.core_domain.repository.PerfumeRepository
+import com.hmoa.core_domain.repository.PerfumerRepository
+import com.hmoa.core_domain.repository.ReportRepository
+import com.hmoa.core_domain.repository.SearchRepository
+import com.hmoa.core_domain.repository.SignupRepository
+import com.hmoa.core_domain.repository.TermRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -78,4 +95,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindTermRepositoryImpl(repositoryImpl: TermRepositoryImpl) : TermRepository
+
+    @Binds
+    @Singleton
+    fun bindMagazineRepositoryImpl(repositoryImpl : MagazineRepositoryImpl) : MagazineRepository
 }

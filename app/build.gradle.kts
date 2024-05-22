6ccbec5f5eb4ee221c7578afb4f61dc6e9341f17
이용인 <yongin0517@gmail.com>
2024-05-22 19:47:50 +0900

    Chore: 버전 코드 3으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.hmoa.app"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

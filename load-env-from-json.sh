306cbeef4c6a5204e162853cfe09871c31628306
이용인 <yongin0517@gmail.com>
2024-06-24 21:17:31 +0900

    Feat: local.properties 속성 추가

--- load-env-from-json.sh
+++ load-env-from-json.sh
@@ -5,6 +5,7 @@ CONFIG_FILE="config.json"
 
 # jq를 사용하여 JSON 파일에서 값을 읽어 환경 변수로 설정
 export NATIVE_APP_KEY=$(jq -r '.["app"]["NATIVE_APP_KEY"]' "$CONFIG_FILE")
+export REDIRECTION_PATH=$(jq -r '.["app"]["REDIRECTION_PATH"]' "$CONFIG_FILE")
 export KEY_ALIAS=$(jq -r '.["app"]["KEY_ALIAS"]' "$CONFIG_FILE")
 export KEY_PASSWORD=$(jq -r '.["app"]["KEY_PASSWORD"]' "$CONFIG_FILE")
 export STORE_FILE=$(jq -r '.["app"]["STORE_FILE"]' "$CONFIG_FILE")

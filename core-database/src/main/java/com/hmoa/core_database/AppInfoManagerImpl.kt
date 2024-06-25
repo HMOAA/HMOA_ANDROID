59a9cc86cfc8342bf48ed8a26b6e6a3e20c689a4
uselessNaming <seohojon@naver.com>
2024-06-25 20:57:00 +0900

    Feat: 알림 수신 여부 이벤트 추가

---
+++ core-database/src/main/java/com/hmoa/core_database/AppInfoManagerImpl.kt
@@ -0,0 +1,30 @@
+package com.hmoa.core_database
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AppInfoManagerImpl @Inject constructor(
+    @ApplicationContext context : Context
+) : AppInfoManager {
+    companion object{
+        private val IS_NOTIFICATION_ENABLED = booleanPreferencesKey("is_notification_enabled")
+    }
+    private val dataStore = context.datastore
+
+    override suspend fun saveNotificationEnabled(isEnabled: Boolean) {
+        dataStore.edit{
+            it[IS_NOTIFICATION_ENABLED] = isEnabled
+        }
+    }
+
+    override suspend fun getNotificationEnabled(): Flow<Boolean> {
+        return dataStore.data.map{
+            it[IS_NOTIFICATION_ENABLED] ?: true
+        }
+    }
+}

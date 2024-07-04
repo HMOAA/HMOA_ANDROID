063c729642d5494e386742d8d3b9556daf399ac6
이용인 <yongin0517@gmail.com>
2024-07-04 21:53:49 +0900

    Feat:선택지 무효화 기능 추가 및 SurveyOptionList로 수정

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/SurveyOptionItem.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/SurveyOptionItem.kt
@@ -3,16 +3,13 @@ package com.hmoa.core_designsystem.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,29 +22,58 @@ import com.hmoa.core_designsystem.theme.CustomColor
 import com.hmoa.core_designsystem.theme.pretendard
 
 @Composable
+fun SurveyOptionList(
+    initValue: String? = null,
+    surveyOptions: List<String>,
+    onButtonClick: (value: String) -> Unit
+) {
+    val surveyOptions = surveyOptions
+    val (selectedOption, onOptionSelected) = remember {
+        val idx = if (initValue == null) 0 else surveyOptions.indexOf(initValue)
+        mutableStateOf(surveyOptions[idx])
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        surveyOptions.forEach {
+            SurveyOptionItem(
+                text = it,
+                onClick = {
+                    onOptionSelected(it)
+                    onButtonClick(it)
+                },
+                isSelected = (it == selectedOption)
+            )
+        }
+    }
+}
+
+@Composable
 fun SurveyOptionItem(text: String, onClick: () -> Unit, isSelected: Boolean) {
-    val isSelected = remember { mutableStateOf(isSelected) }
     Row(
         modifier = Modifier.fillMaxWidth(1f)
             .background(
-                color = if (isSelected.value) CustomColor.gray5 else CustomColor.gray11,
+                color = if (isSelected) CustomColor.gray5 else CustomColor.gray11,
                 shape = RoundedCornerShape(5.dp)
             )
             .padding(horizontal = 16.dp, vertical = 19.dp)
             .clickable {
-                isSelected.value = !isSelected.value
                 onClick()
             },
         verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
     ) {
         Image(
-            painter = if (isSelected.value) painterResource(com.hmoa.core_designsystem.R.drawable.ic_check) else painterResource(
+            painter = if (isSelected) painterResource(com.hmoa.core_designsystem.R.drawable.ic_check) else painterResource(
                 com.hmoa.core_designsystem.R.drawable.ic_check_empty
             ), contentDescription = null, modifier = Modifier.padding(end = 15.dp)
         )
         Text(
             text = text,
-            style = TextStyle(fontSize = 14.sp, fontFamily = pretendard, fontWeight = FontWeight.Medium, color = if (isSelected.value) Color.White else CustomColor.gray5)
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = pretendard,
+                fontWeight = FontWeight.Medium,
+                color = if (isSelected) Color.White else CustomColor.gray5
+            )
         )
     }
 }
@@ -56,21 +82,5 @@ fun SurveyOptionItem(text: String, onClick: () -> Unit, isSelected: Boolean) {
 @Composable
 fun SurveyOptionItemPreview() {
     val seasons = listOf("싱그럽고 활기찬 '봄'", "화창하고 에너지 넘치는 '여름'", "우아하고 고요한 분위기의 '가을'", "차가움과 아늑함이 공존하는 '겨울'")
-    val selectedStates0 = remember { mutableStateOf(false) }
-    val selectedStates1 = remember { mutableStateOf(false) }
-
-    SurveyOptionItem(
-        text = seasons[0],
-        onClick = {
-            selectedStates1.value = false
-        },
-        isSelected = selectedStates0.value
-    )
-    SurveyOptionItem(
-        text = seasons[1],
-        onClick = {
-            selectedStates0.value = false
-        },
-        isSelected = selectedStates1.value
-    )
+    SurveyOptionList(null, seasons, {})
 }

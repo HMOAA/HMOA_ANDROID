b4780b104662b657cc26092d9d605d6eaf6e397c
uselessNaming <seohojon@naver.com>
2024-07-09 15:31:39 +0900

    Feat: 향수 추천 화면 추가

---
+++ feature-hbti/src/main/java/com/hmoa/feature_hbti/screen/PerfumeRecommnedScreen.kt
@@ -0,0 +1,20 @@
+package com.hmoa.feature_hbti.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PerfumeRecommendRoute(){
+
+}
+
+@Composable
+fun PerfumeRecommendScreen(){
+
+}
+
+@Preview
+@Composable
+private fun PreviewScreen(){
+
+}

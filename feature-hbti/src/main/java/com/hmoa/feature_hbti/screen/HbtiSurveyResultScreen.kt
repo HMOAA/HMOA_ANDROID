9f6942364ccacaeb625e852b97a5f12fd2e4ffd4
이용인 <yongin0517@gmail.com>
2024-07-17 02:05:15 +0900

    Feat: HbtiSurveyResult화면 생성

--- feature-hbti/src/main/java/com/hmoa/feature_hbti/screen/HbtiSurveyResultScreen.kt
+++ feature-hbti/src/main/java/com/hmoa/feature_hbti/screen/HbtiSurveyResultScreen.kt
@@ -18,39 +18,64 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hmoa.component.TopBar
 import com.hmoa.core_designsystem.component.Button
+import com.hmoa.core_designsystem.component.ErrorUiSetView
 import com.hmoa.core_designsystem.component.ImageView
 import com.hmoa.core_designsystem.theme.CustomColor
 import com.hmoa.core_designsystem.theme.pretendard
 import com.hmoa.core_model.request.NoteResponseDto
-import com.hmoa.core_model.response.RecommendNotesResponseDto
+import com.hmoa.feature_hbti.viewmodel.HbtiSurveyResultUiState
+import com.hmoa.feature_hbti.viewmodel.HbtiSurveyResultViewmodel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun HbtiSurveyResultRoute() {
-    HbtiSurveyResultScreen()
+fun HbtiSurveyResultRoute(onErrorHandleLoginAgain: () -> Unit, onBackClick: () -> Unit, onNextClick: () -> Unit) {
+    HbtiSurveyResultScreen(onErrorHandleLoginAgain = {}, onBackClick = {}, onNextClick = {})
 }
 
 @Composable
-fun HbtiSurveyResultScreen() {
+private fun HbtiSurveyResultScreen(
+    onErrorHandleLoginAgain: () -> Unit,
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+    viewmodel: HbtiSurveyResultViewmodel = hiltViewModel()
+) {
     var showLoading by remember { mutableStateOf(true) }
+    val uiState by viewmodel.uiState.collectAsStateWithLifecycle()
+    val errorUiState by viewmodel.errorUiState.collectAsStateWithLifecycle()
+
+    ErrorUiSetView(
+        onConfirmClick = { onErrorHandleLoginAgain() },
+        errorUiState = errorUiState,
+        onCloseClick = { onBackClick() }
+    )
 
     LaunchedEffect(Unit) {
         delay(2000) // 2초 지연
         showLoading = false
     }
 
-    if (showLoading) {
-        HbtiSurveyResultLoading()
-    } else {
-        HbtiSurveyResultContent(surveyResult = )
+    when (uiState) {
+        is HbtiSurveyResultUiState.HbtiSurveyResultData -> {
+            if (showLoading) {
+                HbtiSurveyResultLoading((uiState as HbtiSurveyResultUiState.HbtiSurveyResultData).userName)
+            } else {
+                HbtiSurveyResultContent(
+                    surveyResult = (uiState as HbtiSurveyResultUiState.HbtiSurveyResultData).surveyResult,
+                    onNextClick = { onNextClick() })
+            }
+        }
+
+        HbtiSurveyResultUiState.Loading -> HbtiSurveyResultLoading("   ")
     }
 }
 
 @Composable
-fun HbtiSurveyResultLoading() {
+private fun HbtiSurveyResultLoading(userName: String) {
     Column(modifier = Modifier.fillMaxSize()) {
         TopBar(title = "향BTI", titleColor = Color.Black)
         Column(
@@ -63,7 +88,7 @@ fun HbtiSurveyResultLoading() {
                 style = TextStyle(fontFamily = pretendard, fontWeight = FontWeight.Medium, fontSize = 16.sp)
             )
             Text(
-                "땡땡님에게 딱 맞는 향료를\n추천하는 중입니다.",
+                "${userName}님에게 딱 맞는 향료를\n추천하는 중입니다.",
                 modifier = Modifier.padding(top = 15.dp),
                 style = TextStyle(
                     fontFamily = pretendard,
@@ -78,9 +103,9 @@ fun HbtiSurveyResultLoading() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
+private fun HbtiSurveyResultContent(surveyResult: List<NoteResponseDto>, onNextClick: () -> Unit) {
     val scope = rememberCoroutineScope()
-    val pagerState = rememberPagerState(initialPage = 0, pageCount = { surveyResult.recommendNotes.size })
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { surveyResult.size })
     Column(
         modifier = Modifier.fillMaxSize().background(color = Color.White).padding(start = 16.dp, bottom = 40.dp),
         verticalArrangement = Arrangement.SpaceBetween
@@ -92,12 +117,12 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
         )
         Column() {
             Text(
-                "땡땡님에게 딱 맞는 향료는\n'${surveyResult.recommendNotes[0].noteName}'입니다",
+                "땡땡님에게 딱 맞는 향료는\n'${surveyResult[0]}'입니다",
                 style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, fontFamily = pretendard),
                 modifier = Modifier.padding(top = 20.dp, bottom = 12.dp)
             )
             Text(
-                "2위 : ${surveyResult.recommendNotes[1].noteName}",
+                "2위 : ${surveyResult[1].noteName}",
                 style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Medium,
@@ -107,7 +132,7 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
                 modifier = Modifier.padding(bottom = 5.dp)
             )
             Text(
-                "3위 : ${surveyResult.recommendNotes[1].noteName}",
+                "3위 : ${surveyResult[1].noteName}",
                 style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Medium,
@@ -124,7 +149,7 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
                 Column(modifier = Modifier.fillMaxWidth(1f).fillMaxHeight(0.8f).padding(end = 15.dp)) {
                     Column(modifier = Modifier.fillMaxWidth(1f).fillMaxHeight(0.6f)) {
                         ImageView(
-                            imageUrl = surveyResult.recommendNotes[page].notePhotoUrl,
+                            imageUrl = surveyResult[page].notePhotoUrl,
                             width = 1f,
                             height = 1f,
                             contentScale = ContentScale.Crop,
@@ -136,7 +161,7 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
                             .background(color = Color.Black)
                     ) {
                         Text(
-                            "${surveyResult.recommendNotes[page].noteName}",
+                            "${surveyResult[page].noteName}",
                             style = TextStyle(
                                 fontFamily = pretendard,
                                 fontSize = 16.sp,
@@ -146,7 +171,7 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
                             modifier = Modifier.padding(bottom = 7.dp, top = 20.dp).padding(horizontal = 20.dp)
                         )
                         Text(
-                            "${surveyResult.recommendNotes[page].content}",
+                            "${surveyResult[page].content}",
                             style = TextStyle(
                                 fontFamily = pretendard,
                                 fontSize = 12.sp,
@@ -163,7 +188,7 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
                     isEnabled = true,
                     btnText = "다음",
                     onClick = {
-                        scope.launch { pagerState.scrollToPage(pagerState.currentPage + 1) }
+                        scope.launch { onNextClick() }
                     },
                     buttonModifier = Modifier.fillMaxWidth(1f).height(52.dp).background(color = Color.Black),
                     textSize = 18,
@@ -177,28 +202,27 @@ fun HbtiSurveyResultContent(surveyResult: RecommendNotesResponseDto) {
 
 @Preview
 @Composable
-fun HbtiSurveyResultPreview() {
-    val result = RecommendNotesResponseDto(
-        recommendNotes = listOf(
-            NoteResponseDto(
-                noteId = 0,
-                noteName = "시트러스",
-                notePhotoUrl = "",
-                content = "귤, 베르가못, 만다린이 들어간 상큼한 향료로 향수에서 가장 많이 사용되는 노트입니다."
-            ),
-            NoteResponseDto(
-                noteId = 1,
-                noteName = "플로럴",
-                notePhotoUrl = "",
-                content = "네롤리, 화이트 로즈, 핑크 로즈가 들어간 향료로 향수에서 가장 많이 사용되는 노트입니다."
-            ),
-            NoteResponseDto(
-                noteId = 2,
-                noteName = "스파이스",
-                notePhotoUrl = "",
-                content = "넛맥, 블랙페퍼가 들어간 향료로 제가 좋아하는 향료입니다"
-            )
+private fun HbtiSurveyResultPreview() {
+    val result = listOf(
+        NoteResponseDto(
+            noteId = 0,
+            noteName = "시트러스",
+            notePhotoUrl = "",
+            content = "귤, 베르가못, 만다린이 들어간 상큼한 향료로 향수에서 가장 많이 사용되는 노트입니다."
+        ),
+        NoteResponseDto(
+            noteId = 1,
+            noteName = "플로럴",
+            notePhotoUrl = "",
+            content = "네롤리, 화이트 로즈, 핑크 로즈가 들어간 향료로 향수에서 가장 많이 사용되는 노트입니다."
+        ),
+        NoteResponseDto(
+            noteId = 2,
+            noteName = "스파이스",
+            notePhotoUrl = "",
+            content = "넛맥, 블랙페퍼가 들어간 향료로 제가 좋아하는 향료입니다"
         )
     )
-    HbtiSurveyResultContent(result)
+
+    HbtiSurveyResultContent(result, {})
 }
---
+++ feature-hbti/src/main/java/com/hmoa/feature_hbti/viewmodel/HbtiSurveyResultViewmodel.kt
@@ -0,0 +1,103 @@
+package com.hmoa.feature_hbti.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hmoa.core_common.ErrorMessageType
+import com.hmoa.core_common.ErrorUiState
+import com.hmoa.core_common.Result
+import com.hmoa.core_common.asResult
+import com.hmoa.core_domain.repository.MemberRepository
+import com.hmoa.core_domain.repository.SurveyRepository
+import com.hmoa.core_model.request.NoteResponseDto
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HbtiSurveyResultViewmodel @Inject constructor(
+    private val memberRepository: MemberRepository,
+    private val surveyRepository: SurveyRepository
+) : ViewModel() {
+    private var _surveyResultState = MutableStateFlow<List<NoteResponseDto>>(listOf())
+    private var _userNameState = MutableStateFlow<String>("")
+    private var expiredTokenErrorState = MutableStateFlow<Boolean>(false)
+    private var wrongTypeTokenErrorState = MutableStateFlow<Boolean>(false)
+    private var unLoginedErrorState = MutableStateFlow<Boolean>(false)
+    private var generalErrorState = MutableStateFlow<Pair<Boolean, String?>>(Pair(false, null))
+    val errorUiState: StateFlow<ErrorUiState> = combine(
+        expiredTokenErrorState,
+        wrongTypeTokenErrorState,
+        unLoginedErrorState,
+        generalErrorState
+    ) { expiredTokenError, wrongTypeTokenError, unknownError, generalError ->
+        ErrorUiState.ErrorData(
+            expiredTokenError = expiredTokenError,
+            wrongTypeTokenError = wrongTypeTokenError,
+            unknownError = unknownError,
+            generalError = generalError
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = ErrorUiState.Loading
+    )
+    val uiState: StateFlow<HbtiSurveyResultUiState> =
+        combine(_userNameState, _surveyResultState) { userName, surveyResult ->
+            HbtiSurveyResultUiState.HbtiSurveyResultData(
+                userName = userName,
+                surveyResult = surveyResult
+            )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000), initialValue = HbtiSurveyResultUiState.Loading
+        )
+
+    init {
+        viewModelScope.launch {
+            getUserName()
+            getSurveyResult()
+        }
+    }
+
+    suspend fun getUserName() {
+        flow { emit(memberRepository.getMember()) }.asResult().collectLatest { result ->
+            when (result) {
+                is Result.Success -> {
+                    _userNameState.update { result.data.data?.nickname!! }
+                }
+
+                is Result.Error -> when (result.exception.message) {
+                    ErrorMessageType.EXPIRED_TOKEN.message -> {
+                        expiredTokenErrorState.update { true }
+                    }
+
+                    ErrorMessageType.WRONG_TYPE_TOKEN.message -> {
+                        wrongTypeTokenErrorState.update { true }
+                    }
+
+                    ErrorMessageType.UNKNOWN_ERROR.message -> {
+                        unLoginedErrorState.update { true }
+                    }
+
+                    else -> {
+                        generalErrorState.update { Pair(true, result.exception.message) }
+                    }
+                }
+
+                Result.Loading -> HbtiSurveyResultUiState.Loading
+            }
+        }
+    }
+
+    suspend fun getSurveyResult() {
+        val result = surveyRepository.getAllSurveyResult()
+        _surveyResultState.update { result }
+    }
+}
+
+sealed interface HbtiSurveyResultUiState {
+    data object Loading : HbtiSurveyResultUiState
+    data class HbtiSurveyResultData(
+        val userName: String,
+        val surveyResult: List<NoteResponseDto>
+    ) : HbtiSurveyResultUiState
+}

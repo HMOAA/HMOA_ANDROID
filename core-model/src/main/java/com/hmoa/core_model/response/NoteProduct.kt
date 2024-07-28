b511980d7c1725f8be1f1742470eea59b1cfd423
uselessNaming <seohojon@naver.com>
2024-07-28 15:44:42 +0900

    Feat: 멤버 변수 추가

--- core-model/src/main/java/com/hmoa/core_model/response/NoteProduct.kt
+++ core-model/src/main/java/com/hmoa/core_model/response/NoteProduct.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class NoteProduct(
     val notes: List<Note>,
+    val notesCount: Int,
     val price: Int,
     val productId: Int,
     val productName: String,

bf014af06a2d995b7e071f6cb39738b0ede46515
이용인 <yongin0517@gmail.com>
2024-07-29 22:06:20 +0900

    Feat: ErrorUiState 추가 및 유저화면 적용

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/MyPageViewModel.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/viewModel/MyPageViewModel.kt
@@ -29,17 +29,20 @@ class MyPageViewModel @Inject constructor(
     private var expiredTokenErrorState = MutableStateFlow<Boolean>(false)
     private var wrongTypeTokenErrorState = MutableStateFlow<Boolean>(false)
     private var unLoginedErrorState = MutableStateFlow<Boolean>(false)
+    private var memberNotFoundErrorState = MutableStateFlow<Boolean>(false)
     private var generalErrorState = MutableStateFlow<Pair<Boolean, String?>>(Pair(false, null))
     val errorUiState: StateFlow<ErrorUiState> = combine(
         expiredTokenErrorState,
         wrongTypeTokenErrorState,
         unLoginedErrorState,
+        memberNotFoundErrorState,
         generalErrorState
-    ) { expiredTokenError, wrongTypeTokenError, unknownError, generalError ->
+    ) { expiredTokenError, wrongTypeTokenError, unknownError, memberNotFoundError, generalError ->
         ErrorUiState.ErrorData(
             expiredTokenError = expiredTokenError,
             wrongTypeTokenError = wrongTypeTokenError,
             unknownError = unknownError,
+            memberNotFoundError = memberNotFoundError,
             generalError = generalError
         )
     }.stateIn(
@@ -74,6 +77,7 @@ class MyPageViewModel @Inject constructor(
                     ErrorMessageType.EXPIRED_TOKEN.message -> expiredTokenErrorState.update { true }
                     ErrorMessageType.WRONG_TYPE_TOKEN.message -> wrongTypeTokenErrorState.update { true }
                     ErrorMessageType.UNKNOWN_ERROR.message -> unLoginedErrorState.update { true }
+                    ErrorMessageType.MEMBER_NOT_FOUND.message -> memberNotFoundErrorState.update { true }
                     else -> generalErrorState.update { Pair(true, result.exception.message) }
                 }
                 UserInfoUiState.Error

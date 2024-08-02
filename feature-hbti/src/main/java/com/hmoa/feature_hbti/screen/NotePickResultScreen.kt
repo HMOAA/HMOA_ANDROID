2cdd65e758508200d00fd3485bcafd1873f4829b
uselessNaming <seohojon@naver.com>
2024-08-03 00:33:22 +0900

    Feat: ui state 분기

--- feature-hbti/src/main/java/com/hmoa/feature_hbti/screen/NotePickResultScreen.kt
+++ feature-hbti/src/main/java/com/hmoa/feature_hbti/screen/NotePickResultScreen.kt
@@ -21,104 +21,73 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hmoa.component.TopBar
+import com.hmoa.core_common.ErrorUiState
+import com.hmoa.core_designsystem.component.AppLoadingScreen
 import com.hmoa.core_designsystem.component.Button
+import com.hmoa.core_designsystem.component.ErrorUiSetView
 import com.hmoa.core_designsystem.component.NoteSelectedDescription
 import com.hmoa.core_designsystem.theme.CustomFont
 import com.hmoa.core_model.response.Note
 import com.hmoa.core_model.response.NoteProduct
 import com.hmoa.core_model.response.PostNoteSelectedResponseDto
+import com.hmoa.feature_hbti.viewmodel.NotePickResultState
+import com.hmoa.feature_hbti.viewmodel.NotePickResultViewModel
 
 @Composable
 fun NotePickResultRoute(
     productIds: List<Int>,
-    onNavBack: () -> Unit,
-    onNavNext: () -> Unit
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+    viewModel: NotePickResultViewModel = hiltViewModel()
 ){
-    //임의 데이터
-    val testData = PostNoteSelectedResponseDto(
-        noteProducts = listOf(
-            NoteProduct(
-                notes = listOf(
-                    Note(
-                        noteName = "통카빈",
-                        noteContent = "열대 나무 씨앗을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "넛맥",
-                        noteContent = "넛맥이란 식물을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "페퍼",
-                        noteContent = "후추"
-                    )
-                ),
-                notesCount = 3,
-                price = 4800,
-                productId = 0,
-                productName = "프루트",
-                productPhotoUrl = ""
-            ),
-            NoteProduct(
-                notes = listOf(
-                    Note(
-                        noteName = "통카빈",
-                        noteContent = "열대 나무 씨앗을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "넛맥",
-                        noteContent = "넛맥이란 식물을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "페퍼",
-                        noteContent = "후추"
-                    )
-                ),
-                notesCount = 6,
-                price = 4800,
-                productId = 0,
-                productName = "플로럴",
-                productPhotoUrl = ""
-            ),
-            NoteProduct(
-                notes = listOf(
-                    Note(
-                        noteName = "통카빈",
-                        noteContent = "열대 나무 씨앗을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "넛맥",
-                        noteContent = "넛맥이란 식물을 말린 향신료"
-                    ),
-                    Note(
-                        noteName = "페퍼",
-                        noteContent = "후추"
-                    )
-                ),
-                notesCount = 6,
-                price = 6000,
-                productId = 0,
-                productName = "시트러스",
-                productPhotoUrl = ""
-            )
-        ),
-        totalPrice = 15600
-    )
+    viewModel.setNoteIds(productIds)
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    val errState = viewModel.errorUiState.collectAsStateWithLifecycle()
     NotePickResultScreen(
-        selectedNotes = testData, //navigation을 통해 전달 받은 list로 하는 게 좋을 것 같군
-        onNavBack = onNavBack,
-        onNavNext = onNavNext
+        uiState = uiState.value,
+        errState = errState.value,
+        onBackClick = onBackClick,
+        onNextClick = onNextClick
     )
 }
 
 @Composable
 fun NotePickResultScreen(
-    selectedNotes: PostNoteSelectedResponseDto,
-    onNavBack: () -> Unit,
-    onNavNext: () -> Unit
+    uiState: NotePickResultState,
+    errState: ErrorUiState,
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit
 ){
-    val notes = selectedNotes.noteProducts
-    val totalPrice = selectedNotes.totalPrice
+    when(uiState){
+        NotePickResultState.Loading -> AppLoadingScreen()
+        is NotePickResultState.Success -> {
+            NotePickResultMainContent(
+                notes = uiState.result.noteProducts,
+                totalPrice = uiState.result.totalPrice,
+                onBackClick = onBackClick,
+                onNextClick = onNextClick,
+            )
+        }
+        is NotePickResultState.Error -> {
+            ErrorUiSetView(
+                onConfirmClick = onBackClick, /**여기 Hbti Screen으로 navigation 하는 건 어떨까*/
+                errorUiState = errState,
+                onCloseClick = onBackClick /**여기 Hbti Screen으로 navigation 하는 건 어떨까*/
+            )
+        }
+    }
+}
+
+@Composable
+private fun NotePickResultMainContent(
+    notes: List<NoteProduct>,
+    totalPrice: Int,
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+){
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -127,7 +96,7 @@ fun NotePickResultScreen(
         TopBar(
             title = "향BTI",
             navIcon = painterResource(com.hmoa.core_designsystem.R.drawable.ic_back),
-            onNavClick = onNavBack
+            onNavClick = onBackClick
         )
         Spacer(Modifier.height(20.dp))
         Column(
@@ -165,11 +134,12 @@ fun NotePickResultScreen(
             )
             Spacer(Modifier.weight(1f))
             Button(
-                buttonModifier = Modifier.fillMaxWidth()
+                buttonModifier = Modifier
+                    .fillMaxWidth()
                     .height(52.dp),
                 isEnabled = true,
                 btnText = "다음",
-                onClick = onNavNext,
+                onClick = onNextClick,
                 radious = 5
             )
             Spacer(Modifier.height(40.dp))
@@ -249,8 +219,9 @@ private fun UITest(){
         totalPrice = 15600
     )
     NotePickResultScreen(
-        selectedNotes = testData,
-        onNavBack = {},
-        onNavNext = {}
+        uiState = NotePickResultState.Success(result = testData),
+        errState = ErrorUiState.Loading,
+        onBackClick = {},
+        onNextClick = {}
     )
 }

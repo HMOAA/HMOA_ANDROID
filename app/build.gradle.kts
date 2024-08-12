ffe8efde1f3140f96b1e4cc877039c46f12306e7
이용인 <yongin0517@gmail.com>
2024-08-12 19:06:18 +0900

    Revert "Chore: 버전코드 1로 초기화"
    This reverts commit 529e65abe5c96a90943b6ffd466ae5a4742570ef.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.hyangmoa.app"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
+        versionCode = 18
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

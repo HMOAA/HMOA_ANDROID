5d793529ed4094f3c725990ddeecf854f4764c6a
이용인 <yongin0517@gmail.com>
2024-08-27 01:59:10 +0900

    Test: HbtiSurveyResultScreen 로딩화면 존재확인 테스트

--- feature-hbti/src/androidTest/java/com/hmoa/feature_hbti/HbtiSurveyResultScreenTest.kt
+++ feature-hbti/src/androidTest/java/com/hmoa/feature_hbti/HbtiSurveyResultScreenTest.kt
@@ -1,20 +1,25 @@
 package com.hmoa.feature_hbti
 
 import ResultResponse
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.hmoa.core_common.ErrorUiState
+import com.hmoa.core_domain.repository.MemberRepository
+import com.hmoa.core_domain.repository.SurveyRepository
+import com.hmoa.core_model.request.NoteResponseDto
 import com.hmoa.core_model.response.MemberResponseDto
 import com.hmoa.feature_hbti.screen.HbtiSurveyResultScreen
+import com.hmoa.feature_hbti.viewmodel.HbtiSurveyResultUiState
 import com.hmoa.feature_hbti.viewmodel.HbtiSurveyResultViewmodel
 import junit.framework.TestCase
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.Mockito.mock
 
 
 /**
@@ -26,26 +31,36 @@ import org.mockito.Mockito
 class HbtiSurveyResultScreenTest : TestCase() {
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<TestActivity>()
+    val composeTestRule = createComposeRule()
+    val memberRepository = mock(MemberRepository::class.java)
+    val surveyRepository = mock(SurveyRepository::class.java)
     lateinit var viewModel: HbtiSurveyResultViewmodel
 
-//    val memberId: Int,
-//    val memberImageUrl: String,
-//    val nickname: String,
-//    val provider: String,
-//    val sex: Boolean
-    companion object{
+    companion object {
         val fakeMember = ResultResponse<MemberResponseDto>(
-            data = MemberResponseDto(age = 20, memberId = 0, memberImageUrl = "fakeUrl", nickname="tester", provider = "GOOGLE", sex = true),
+            data = MemberResponseDto(
+                age = 20,
+                memberId = 0,
+                memberImageUrl = "fakeUrl",
+                nickname = "테스터",
+                provider = "GOOGLE",
+                sex = true
+            ),
             errorMessage = null
         )
-        val fakeSurveyResult = ResultResponse<>
+        val fakeSurveyResult = listOf(
+            NoteResponseDto(content = "스파이스 어쩌구는 뭐뭐다", noteName = "스파이스", noteId = 0, notePhotoUrl = "spiceUrl"),
+            NoteResponseDto(content = "그린 어쩌구는 뭐뭐다", noteName = "그린", noteId = 1, notePhotoUrl = "greenUrl"),
+            NoteResponseDto(content = "머스크 어쩌구는 뭐뭐다", noteName = "머스크", noteId = 2, notePhotoUrl = "muskUrl")
+        )
     }
+
     @Before
-    override fun setUp() {
+    public override fun setUp() {
         runBlocking {
-            Mockito.`when`(viewModel.getUserName()).thenReturn()
-            Mockito.`when`(viewModel.getSurveyResult()).thenReturn()
+            Mockito.`when`(memberRepository.getMember()).thenReturn(fakeMember)
+            Mockito.`when`(surveyRepository.getAllSurveyResult()).thenReturn(fakeSurveyResult)
+            viewModel = HbtiSurveyResultViewmodel(memberRepository, surveyRepository)
         }
     }
 
@@ -56,52 +71,12 @@ class HbtiSurveyResultScreenTest : TestCase() {
                 onErrorHandleLoginAgain = {},
                 onBackClick = {},
                 onHbtiProcessClick = {},
-                viewmodel = FakeHbtiSurveyResultViewModel().apply {
-                    setUiState(HbtiSurveyResultUiState.Loading)
-                }
+                uiState = HbtiSurveyResultUiState.Loading,
+                errorUiState = ErrorUiState.Loading,
+                showLoading = true
             )
         }
-
-        composeTestRule.onNodeWithText("   ") // 로딩 화면에서의 텍스트
-            .assertExists()
-    }
-
-    @Test
-    fun testSurveyResultDataState() {
-        composeTestRule.setContent {
-            HbtiSurveyResultScreen(
-                onErrorHandleLoginAgain = {},
-                onBackClick = {},
-                onHbtiProcessClick = {},
-                viewmodel = FakeHbtiSurveyResultViewModel().apply {
-                    setUiState(HbtiSurveyResultUiState.HbtiSurveyResultData("John Doe", "Sample Result"))
-                }
-            )
-        }
-
-        composeTestRule.onNodeWithText("John Doe")
-            .assertExists()
-
-        composeTestRule.onNodeWithText("Sample Result")
-            .assertExists()
-    }
-
-    @Test
-    fun testLoadingToContentTransition() {
-        composeTestRule.setContent {
-            HbtiSurveyResultScreen(
-                onErrorHandleLoginAgain = {},
-                onBackClick = {},
-                onHbtiProcessClick = {},
-                viewmodel = FakeHbtiSurveyResultViewModel().apply {
-                    setUiState(HbtiSurveyResultUiState.HbtiSurveyResultData("John Doe", "Sample Result"))
-                }
-            )
-        }
-
-        composeTestRule.mainClock.advanceTimeBy(2000) // 2초 후 상태 변화
-
-        composeTestRule.onNodeWithText("John Doe")
-            .assertExists()
+        // Assert
+        composeTestRule.onNodeWithTag("HbtiSurveyResultLoading").assertExists()
     }
 }

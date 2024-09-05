e462bacbdfcd9ec643b80c5006d79bc3ef26e7a3
uselessNaming <seohojon@naver.com>
2024-09-05 17:27:10 +0900

    Design: focus 잡힐 시 placeholder 제거

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/CustomOutlinedTextField.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/CustomOutlinedTextField.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
@@ -32,9 +37,11 @@ fun CustomOutlinedTextField(
     borderShape: Shape,
     padding: PaddingValues,
 ){
+    var isFocused by remember{mutableStateOf(false)}
     BasicTextField(
         modifier = modifier.border(width = borderWidth, color = borderColor, shape = borderShape)
-            .padding(paddingValues = padding),
+            .padding(paddingValues = padding)
+            .onFocusChanged {isFocused = it.isFocused},
         value = value,
         onValueChange = onValueChanged,
         textStyle = TextStyle(
@@ -48,7 +55,7 @@ fun CustomOutlinedTextField(
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ){
-                if (placeHolder != null && value.isEmpty()){
+                if (!isFocused && placeHolder != null && value.isEmpty()){
                     Text(
                         text = placeHolder,
                         fontSize = fontSize,

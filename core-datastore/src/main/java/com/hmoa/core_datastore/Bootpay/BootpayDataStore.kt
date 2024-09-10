cf56ccb5ee40f801ca10259af1303374dd145153
uselessNaming <seohojon@naver.com>
2024-09-10 19:32:31 +0900

    Feat: bootpay datastore 추가

---
+++ core-datastore/src/main/java/com/hmoa/core_datastore/Bootpay/BootpayDataStore.kt
@@ -0,0 +1,11 @@
+package com.hmoa.core_datastore.Bootpay
+
+import ResultResponse
+import com.hmoa.core_model.request.CancelBootpayRequestDto
+import com.hmoa.core_model.request.ConfirmBootpayRequestDto
+import com.hmoa.core_model.response.DataResponseDto
+
+interface BootpayDataStore {
+    suspend fun postConfirm(requestDto: ConfirmBootpayRequestDto): ResultResponse<DataResponseDto<Any>>
+    suspend fun postCancel(requestDto: CancelBootpayRequestDto): ResultResponse<DataResponseDto<Any>>
+}

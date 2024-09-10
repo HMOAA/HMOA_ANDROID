6c4be3d1fa53dd8bd2ac54795082c4a512a4d774
uselessNaming <seohojon@naver.com>
2024-09-10 20:02:52 +0900

    Feat: boot pay analytics 초기화

--- app/src/main/java/com/hmoa/app/MainActivity.kt
+++ app/src/main/java/com/hmoa/app/MainActivity.kt
@@ -55,6 +55,10 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kr.co.bootpay.android.BootpayAnalytics
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -93,6 +97,7 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         requestNotificationPermission()
         initFirebaseSetting()
+        BootpayAnalytics.init(this, BuildConfig.BOOTPAY_APPLICATION_ID)
 
         lifecycleScope.launch {
             val currentJob = coroutineContext.job

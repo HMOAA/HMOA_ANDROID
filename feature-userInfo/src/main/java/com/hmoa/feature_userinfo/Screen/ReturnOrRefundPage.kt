ec2e9f8451cf73f093d2b5b5e19e3d1185e6d061
uselessNaming <seohojon@naver.com>
2024-09-14 21:52:09 +0900

    Feat: kakao 1대1 채팅 이벤트 추가

--- feature-userInfo/src/main/java/com/hmoa/feature_userinfo/Screen/ReturnOrRefundPage.kt
+++ feature-userInfo/src/main/java/com/hmoa/feature_userinfo/Screen/ReturnOrRefundPage.kt
@@ -1,5 +1,6 @@
 package com.hmoa.feature_userinfo.Screen
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,19 +28,35 @@ import com.hmoa.core_designsystem.component.Button
 import com.hmoa.core_designsystem.component.NoteListItem
 import com.hmoa.core_designsystem.theme.CustomColor
 import com.hmoa.core_designsystem.theme.CustomFont
+import com.hmoa.feature_userinfo.BuildConfig
+import com.kakao.sdk.talk.TalkApiClient
 
 @Composable
 fun ReturnOrRefundRoute(
     type: String
 ){
-    ReturnOrRefundScreen(type = type)
+    val context = LocalContext.current
+    val onChatClick = {
+        TalkApiClient.instance.chatChannel(context, BuildConfig.KAKAO_CHAT_PROFILE) { err ->
+            if (err != null) {
+                Toast.makeText(context, "향모아 챗봇 오류가 발생했습니다:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    ReturnOrRefundScreen(
+        type = type,
+        onChatClick = onChatClick
+    )
 }
 
 @Composable
-fun ReturnOrRefundScreen(type: String){
+fun ReturnOrRefundScreen(
+    type: String,
+    onChatClick: () -> Unit
+){
     val title = if(type == "refund") "환불 신청" else "반품 신청"
     val buttonText = if(type == "refund") "환불 신청" else "반품 신청 (1대1 문의)"
-    val buttonEvent = if(type == "refund") {/** 환불 신청 api */} else {/** 카카오톡 1대1 연결 */}
+    val buttonEvent = if(type == "refund") {/** 환불 신청 api */} else onChatClick()
     val dummyData = listOf(0)
     val totalPrice = 15000
     val totalOrderPrice = 12000
@@ -169,6 +187,7 @@ fun ReturnOrRefundScreen(type: String){
 @Composable
 private fun ReturnOrRefundUITest(){
     ReturnOrRefundScreen(
-        type = "refund"
+        type = "refund",
+        onChatClick = {}
     )
 }

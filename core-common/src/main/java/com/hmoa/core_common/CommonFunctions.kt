acb132fd2a4ceb5a4aaf22e2b0fe6eab3a9f7e10
uselessNaming <seohojon@naver.com>
2024-09-24 02:16:34 +0900

    Feat: 배송 상태 번역 함수 추가

--- core-common/src/main/java/com/hmoa/core_common/CommonFunctions.kt
+++ core-common/src/main/java/com/hmoa/core_common/CommonFunctions.kt
@@ -1,5 +1,7 @@
 package com.hmoa.core_common
 
+import com.hmoa.core_model.data.OrderStatus
+
 fun formatWon(value: Int): String = String.format("%,d", value)
 
 fun concatWithComma(values: List<String>): String{
@@ -11,4 +13,15 @@ fun concatWithComma(values: List<String>): String{
         }
     }
     return result
+}
+
+fun OrderStatus.toDisplayString(): String{
+    return when(this){
+        OrderStatus.CREATED -> "주문 생성"
+        OrderStatus.PAY_FAILED -> "결제 실패"
+        OrderStatus.PAY_COMPLETE -> "결제 완료"
+        OrderStatus.SHIPPING_COMPLETE -> "배송 완료"
+        OrderStatus.SHIPPING_PROGRESS -> "배송 중"
+        OrderStatus.PURCHASE_CONFIRMATION -> "구매 확정"
+    }
 }

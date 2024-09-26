129c0218b0c67a6d9b17531bf542d61330b1523b
uselessNaming <seohojon@naver.com>
2024-09-26 22:18:19 +0900

    Feat: order status 반품 완료 상태 추가

--- core-model/src/main/java/com/hmoa/core_model/data/OrderStatus.kt
+++ core-model/src/main/java/com/hmoa/core_model/data/OrderStatus.kt
@@ -3,9 +3,10 @@ package com.hmoa.core_model.data
 enum class OrderStatus {
     CREATED, //주문 생성
     PAY_FAILED, //결제 실패
-    PAY_CANCEL, //결제 취소
+    PAY_CANCEL, //결제 취소 (환불 완료)
     PAY_COMPLETE, //결제 완료
     SHIPPING_PROGRESS, //배송 중
     SHIPPING_COMPLETE, //배송 완료
-    PURCHASE_CONFIRMATION //구매 확정
+    PURCHASE_CONFIRMATION, //구매 확정
+    RETURN_COMPLETE // 반품 완료
 }

fbbfede3cc6730cc2df5836cb5e7d3c999e19e8f
uselessNaming <seohojon@naver.com>
2024-09-26 22:52:01 +0900

    Design: 버튼 삭제

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/EmptyDataPage.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/EmptyDataPage.kt
@@ -1,6 +1,7 @@
 package com.hmoa.core_designsystem.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,18 +19,14 @@ import com.hmoa.core_designsystem.R
 import com.hmoa.core_designsystem.theme.CustomFont
 
 @Composable
-fun EmptyDataPage(
-    mainText: String,
-    buttonText: String,
-    onClick: () -> Unit,
-){
+fun EmptyDataPage(mainText: String){
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-        Spacer(Modifier.weight(1f))
         Image(
             modifier = Modifier.size(110.dp),
             painter = painterResource(R.drawable.ic_app_default_1),
@@ -40,14 +37,6 @@ fun EmptyDataPage(
             text = mainText,
             fontSize = 22.sp,
             fontFamily = CustomFont.bold,
-        )
-        Spacer(Modifier.weight(1f))
-        Button(
-            textSize = 15,
-            radious = 5,
-            isEnabled = true,
-            btnText = buttonText,
-            onClick = onClick
         )
     }
 }

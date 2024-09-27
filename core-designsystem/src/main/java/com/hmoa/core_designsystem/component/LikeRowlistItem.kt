e407873268f1dfa79bafe234a15ef7be5c8d7091
uselessNaming <seohojon@naver.com>
2024-09-27 21:05:57 +0900

    Rename: navigation 이벤트 명 변경

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeRowlistItem.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeRowlistItem.kt
@@ -3,7 +3,17 @@ package com.hmoa.core_designsystem.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -16,9 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hmoa.core_designsystem.theme.pretendard
@@ -31,7 +39,7 @@ fun LikeRowItem(
     itemNameKo: String,
     itemNameEng: String,
     onClickClose: () -> Unit,
-    onNavPerfumeDesc: () -> Unit,
+    navPerfumeDesc: () -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -49,7 +57,7 @@ fun LikeRowItem(
                 spotColor = Color(0x33000000),
                 ambientColor = Color(0x33000000)
             ).clickable {
-                onNavPerfumeDesc()
+                navPerfumeDesc()
             },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {

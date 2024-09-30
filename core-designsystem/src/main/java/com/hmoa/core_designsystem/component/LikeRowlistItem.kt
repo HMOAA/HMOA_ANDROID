89b68ade35196c55ad6bfefaa0e8c8be5df312cd
이용인 <yongin0517@gmail.com>
2024-10-01 01:52:20 +0900

    Design: 패딩 및 글자 위치 수정

--- core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeRowlistItem.kt
+++ core-designsystem/src/main/java/com/hmoa/core_designsystem/component/LikeRowlistItem.kt
@@ -3,17 +3,7 @@ package com.hmoa.core_designsystem.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -27,6 +17,8 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hmoa.core_designsystem.theme.pretendard
@@ -61,6 +53,7 @@ fun LikeRowItem(
                 onNavPerfumeDesc()
             },
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         Row(
             modifier = Modifier
@@ -99,49 +92,56 @@ fun LikeRowItem(
 
             Spacer(Modifier.width(16.dp))
         }
-        Spacer(Modifier.weight(1f))
-        Box(
-            modifier = Modifier.size(120.dp).background(color = Color.White)
-        ) {
-            ImageView(
-                imageUrl = itemPicture,
-                width = 1f,
-                height = 1f,
-                backgroundColor = Color.White,
-                contentScale = ContentScale.Fit
-            )
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom, modifier = Modifier.fillMaxWidth().fillMaxHeight(0.8f).background(color = Color.White)) {
+            Box(
+                modifier = Modifier.fillMaxSize(0.6f).background(color = Color.White)
+            ) {
+                ImageView(
+                    imageUrl = itemPicture,
+                    width = 1f,
+                    height = 1f,
+                    backgroundColor = Color.White,
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Spacer(Modifier.height(20.dp))
+            Column(horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxWidth().padding(horizontal = 24.dp)) {
+                Text(
+                    text = itemNameKo,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = pretendard,
+                    lineHeight = 14.sp,
+                    color = Color.Black,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
+                )
+
+                Spacer(Modifier.height(8.dp))
+
+                Text(
+                    text = itemNameEng,
+                    fontSize = 12.sp,
+                    lineHeight = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = pretendard,
+                    color = Color.Black,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
+                )
+            }
         }
-        Spacer(Modifier.height(20.dp))
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 24.dp)
+                .padding(horizontal = 24.dp).padding(bottom = 24.dp)
+                .background(color = Color.White),
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(
-                text = itemNameKo,
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Normal,
-                fontFamily = pretendard,
-                lineHeight = 14.sp,
-                color = Color.Black
-            )
-
-            Spacer(Modifier.height(8.dp))
-
-            Text(
-                text = itemNameEng,
-                fontSize = 12.sp,
-                lineHeight = 12.sp,
-                fontWeight = FontWeight.Normal,
-                fontFamily = pretendard,
-                color = Color.Black
-            )
-
-            Spacer(Modifier.height(28.dp))
-
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                modifier = Modifier.fillMaxWidth().background(color = Color.White),
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Text(
                     text = "Price",
@@ -157,10 +157,36 @@ fun LikeRowItem(
                     lineHeight = 14.sp,
                     fontWeight = FontWeight.Normal,
                     fontFamily = pretendard,
-                    color = Color.Black
+                    color = Color.Black,
                 )
             }
         }
-        Spacer(Modifier.weight(1f))
+    }
+}
+
+@Preview
+@Composable
+fun LikeRowItemPreview(){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        LikeRowItem(
+            brand = "조말론",
+            itemPicture = "?",
+            price = "50000",
+            itemNameKo = "우드 세이지 앤 씨 솔트 코롱",
+            itemNameEng = "Wood Sage & Sea Salt Cologne",
+            onClickClose = {},
+            onNavPerfumeDesc = {},
+            isCloseButtonExist = false
+        )
+        LikeRowItem(
+            brand = "구찌",
+            itemPicture = "?",
+            price = "50000",
+            itemNameKo = "[단종] 플로라 바이 구찌 오 드 퍼퓸",
+            itemNameEng = "[Discontinued] Flora by Gucci Eau de Parfum",
+            onClickClose = {},
+            onNavPerfumeDesc = {},
+            isCloseButtonExist = false
+        )
     }
 }

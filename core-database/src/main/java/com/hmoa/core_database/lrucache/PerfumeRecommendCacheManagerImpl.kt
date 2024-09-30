3831ac76ae2b6be87709db39481f40dccee54f1b
이용인 <yongin0517@gmail.com>
2024-10-01 01:51:46 +0900

    Feat: 향수추천 결과 캐시 추가

--- core-database/src/main/java/com/hmoa/core_database/lrucache/PerfumeRecommendCacheManagerImpl.kt
+++ core-database/src/main/java/com/hmoa/core_database/lrucache/PerfumeRecommendCacheManagerImpl.kt
@@ -6,16 +6,21 @@ import javax.inject.Inject
 
 class PerfumeRecommendCacheManagerImpl @Inject constructor() : PerfumeRecommendCacheManager {
     // 클래스 멤버로 LruCache 선언
-    private val cacheSize = 4 * 1024 * 1024 // 4MB
+    private val cacheSize = 4 * 1024 * 1024 // 4MB...적당한가??
     private val perfumeRecommendsResultCache = LruCache<String, PerfumeRecommendsResponseDto>(cacheSize)
-
-    // 데이터를 캐시에 저장
-    override fun savePerfumeRecommendsResult(dto: PerfumeRecommendsResponseDto) {
-        perfumeRecommendsResultCache.put(CacheKey.PerfumeRecommendsResult.name, dto)
+    override fun saveNoteSortedPerfumeRecommendsResult(dto: PerfumeRecommendsResponseDto) {
+        perfumeRecommendsResultCache.put(CacheKey.NoteSortedPerfumeRecommendsResult.name, dto)
     }
 
-    // 캐시에서 데이터 가져오기
-    override fun getPerfumeRecommendsResult(): PerfumeRecommendsResponseDto? {
-        return perfumeRecommendsResultCache.get(CacheKey.PerfumeRecommendsResult.name)
+    override fun getNoteSortedPerfumeRecommendsResult(): PerfumeRecommendsResponseDto? {
+        return perfumeRecommendsResultCache.get(CacheKey.PriceSortedPerfumeRecommendsResult.name)
+    }
+
+    override fun savePriceSortedPerfumeRecommendsResult(dto: PerfumeRecommendsResponseDto) {
+        perfumeRecommendsResultCache.put(CacheKey.PriceSortedPerfumeRecommendsResult.name, dto)
+    }
+
+    override fun getPriceSortedPerfumeRecommendsResult(): PerfumeRecommendsResponseDto? {
+        return perfumeRecommendsResultCache.get(CacheKey.PriceSortedPerfumeRecommendsResult.name)
     }
 }

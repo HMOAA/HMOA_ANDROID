a56cf26559813e0b716054d7a9f4e280494dc7c2
uselessNaming <seohojon@naver.com>
2024-10-14 13:48:20 +0900

    Chore: material 의존성 추가

--- feature-hbti/build.gradle.kts
+++ feature-hbti/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
 
     implementation("androidx.compose.ui:ui:1.1.0")
     implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material:material:1.2.0-beta02")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.navigation:navigation-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")

5b2e6120c48ace3e4dd3094b8b31fbbbe68b83ce
uselessNaming <seohojon@naver.com>
2024-10-16 12:05:53 +0900

    Feat: 개별 navigation 람다 적용

--- core-model/src/main/java/com/hmoa/core_model/response/ReviewResponseDto.kt
+++ core-model/src/main/java/com/hmoa/core_model/response/ReviewResponseDto.kt
@@ -4,15 +4,15 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewResponseDto(
-    val hbtiReviewId: Int,
-    val profileImgUrl: String,
     val author: String,
     val content: String,
-    val imagesCount: Int,
-    val hbtiPhotos: List<Photo>,
     val createdAt: String,
-    val isWrited: Boolean,
+    val hbtiPhotos: List<Photo>,
+    val hbtiReviewId: Int,
     val heartCount: Int,
+    val imagesCount: Int,
     val isLiked: Boolean,
-    val orderTitle: String
+    val isWrited: Boolean,
+    val orderTitle: String,
+    val profileImgUrl: String,
 )

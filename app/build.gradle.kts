c835223c6ec42a8886e4756ee726b602ceade91e
이용인 <yongin0517@gmail.com>
2024-10-31 03:46:31 +0900

    Chore: 버전 코드 변경 (24 -> 25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.hmoa.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
+        versionCode = 25
         versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

154fab6b4048feca514e9f48b04321d3d94a4268
이용인 <yongin0517@gmail.com>
2024-12-15 03:57:23 +0900

    Chore: Immutable컬렉션 및 stable marker 라이브러리 추가

--- core-model/build.gradle.kts
+++ core-model/build.gradle.kts
@@ -21,11 +21,14 @@ tasks {
 
 dependencies {
     val kotlinx_version = "1.5.0"
+    val kotlinx_collections_immutable_version = "0.3.8"
+    
     implementation("io.ktor:ktor-client-serialization:2.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_version")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
     implementation(kotlin("stdlib"))
-
+    compileOnly("com.github.skydoves:compose-stable-marker:1.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:$kotlinx_collections_immutable_version")
 }

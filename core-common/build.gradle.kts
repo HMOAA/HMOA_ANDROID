28ba38877b8a2b7c91cf1b5d380c04a24ab851dc
uselessNaming <seohojon@naver.com>
2024-12-18 21:20:05 +0900

    Chore: 버전 카탈로그 적용

--- core-common/build.gradle.kts
+++ core-common/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -32,10 +32,7 @@ android {
 dependencies {
     implementation(project(":core-model"))
 
-    implementation(project(":core-model"))
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation(libs.bundles.basic)
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("junit:junit:4.13.2")

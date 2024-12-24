f9700b24401c7554fb9a1a4f7425beabf3c3a25d
이용인 <yongin0517@gmail.com>
2024-12-24 19:55:21 +0900

    Refactor: 기존 PagingSource -> 제네릭 PerfumePagingSource로 리팩터링

---
+++ feature-home/src/main/java/com/hmoa/feature_home/PerfumePagingSource.kt
@@ -0,0 +1,36 @@
+package com.hmoa.feature_home
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class PerfumePagingSource<T : Any, R>(
+    private val fetcher: suspend (Int) -> R,
+    private val mapper: (R) -> List<T>
+) : PagingSource<Int, T>() {
+
+    override fun getRefreshKey(state: PagingState<Int, T>): Int? {
+        return state.anchorPosition?.let {
+            state.closestPageToPosition(it)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
+        val pageNumber = params.key ?: 0
+        return try {
+            val response = fetcher(pageNumber)
+            val data = mapper(response)
+
+            val prevKey = if (pageNumber > 0) pageNumber - 1 else null
+            val nextKey = if (data.isEmpty()) null else pageNumber + 1
+
+            LoadResult.Page(
+                data = data,
+                prevKey = prevKey,
+                nextKey = nextKey
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
